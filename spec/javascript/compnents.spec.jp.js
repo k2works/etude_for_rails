f7bd109a3c9defcca960187f79ee9600e790b192
k2works <kakimomokuri@gmail.com>
2017-04-25 18:05:40 +0900

    構築 JavaScriptテスト環境のセットアップ
    ・Mochaインストール
    ・ES6でテストを記述できるように設定

---
+++ spec/javascript/compnents.spec.jp.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import assert from 'power-assert';
+
+describe('the environment', () => {
+    it('works, hopefully', () => {
+    expect(true).to.be.true;
+    assert(true);
+    });
+});
+
