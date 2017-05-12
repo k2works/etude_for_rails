5af8f9c74f5b56a40c0f7b97ffc790dc10f37b8a
k2works <kakimomokuri@gmail.com>
2017-05-12 15:05:45 +0900

    TDDのプロセスを開始する #1
    最初のテストを記述してテストを通るようにする。

--- test/javascript/compnents.spec.jp.js
+++ test/javascript/compnents.spec.jp.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import { expect } from 'chai';
-import assert from 'power-assert';
+import { shallow, mount } from 'enzyme';
+import { BeerListContainer } from '../../app/javascript/packs/beer_lists/components';
+import { InputArea, BeerList } from '../../app/javascript/packs/beer_lists/components';
 
-describe('the environment', () => {
-    it('works, hopefully', () => {
-        expect(true).to.be.true;
-        assert(true);
+describe('BeerListContainer', () => {
+    it('should render InputArea and BeerList', () => {
+        const wrapper = shallow(<BeerListContainer/>);
+        expect(wrapper.containsAllMatchingElements([
+            <InputArea/>,
+            <BeerList/>
+        ])).to.equal(true);
     });
-});
+})
