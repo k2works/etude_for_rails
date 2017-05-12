081a1f00af92af5816fc76532d5029886361394e
k2works <kakimomokuri@gmail.com>
2017-05-12 16:04:36 +0900

    テスト8： 「Add」ボタンを有効化する #1

--- test/javascript/compnents.spec.jp.js
+++ test/javascript/compnents.spec.jp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { spy } from 'sinon';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import { BeerListContainer } from '../../app/javascript/packs/beer_lists/components';
@@ -49,5 +50,17 @@ describe('InputArea', () => {
         input.simulate('change', {target: {value: 'Resin'}});
         expect(wrapper.state('text')).to.equal('Resin');
         expect(input.prop('value')).to.equal('Resin');
+    });
+    it('should call onSubmit when Add is clicked', () => {
+        const addItemSpy = spy();
+        const wrapper = shallow(<InputArea onSubmit={addItemSpy}/>);
+        wrapper.setState({text:'Octoberfest'});
+        const addButton = wrapper.find('button');
+
+        addButton.simulate('click');
+
+        expect(addItemSpy.calledOnce).to.equal(true)
+
+        expect(addItemSpy.calledWith('Octoberfest')).to.equal(true);
     });
 });
