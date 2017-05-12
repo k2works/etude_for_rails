f0c767b3e68198379523f8c23302bfee46eadcf1
k2works <kakimomokuri@gmail.com>
2017-05-12 15:34:36 +0900

    テスト 4： 関数を渡す #1

--- app/javascript/packs/beer_lists/components.js
+++ app/javascript/packs/beer_lists/components.js
@@ -16,7 +16,7 @@ export class BeerListContainer extends Component {
     render() {
         return(
             <div>
-                <InputArea/>
+                <InputArea onSubmit={this.addItem}/>
                 <BeerList/>
             </div>
         );
@@ -28,6 +28,10 @@ export class InputArea extends Component {
         return <input/>
     }
 }
+
+InputArea.PropTypes = {
+    onSubmit: React.PropTypes.func.isRequired
+};
 
 export class BeerList extends Component {
     render() {
