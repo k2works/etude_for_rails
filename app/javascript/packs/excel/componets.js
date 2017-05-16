99c4b5cf1fd4c5f7157e8435b0bf552aaecd5e0b
k2works <kakimomokuri@gmail.com>
2017-05-16 14:39:11 +0900

    3.7.3 データの保存 #6

--- app/javascript/packs/excel/componets.js
+++ app/javascript/packs/excel/componets.js
@@ -47,6 +47,17 @@ var Excel = React.createClass({
         }});
     },
 
+    _save: function (e) {
+        e.preventDefault();
+        var input = e.target.firstChild;
+        var data = this.state.data.slice();
+        data[this.state.edit.row][this.state.edit.cell] = input.value;
+        this.setState({
+            edit: null,
+            data: data,
+        })
+    },
+
     render: function() {
         return (
             React.DOM.table(null,
