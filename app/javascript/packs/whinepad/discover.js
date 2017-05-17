833c7035866009af3b66a8c095489ac395b4f9c6
k2works <kakimomokuri@gmail.com>
2017-05-17 19:39:29 +0900

    6.2.3 <Button>コンポーネント #7

--- app/javascript/packs/whinepad/discover.js
+++ app/javascript/packs/whinepad/discover.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import styles from './css/discover.scss';
+import Button from './components/Button';
 import Logo from './components/Logo';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -14,6 +15,12 @@ ReactDOM.render(
         <div className={styles.Logo}>
             <Logo/>
         </div>
+
+        <h2>Button</h2>
+        <div>onClickが指定されたButton: <Button onClick={() => alert('クリックされました')}>クリック</Button></div>
+        <div>hrefが指定されたButton: <Button href="http://reactjs.com">フォローする</Button></div>
+        <div>クラス名が指定されたButton: <Button className="custom">何もしません</Button></div>
+
         {/* その他のコンポーネントはここに追加されます... */}
     </div>,
     document.getElementById('pad')
