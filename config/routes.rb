480907186a2a2dac8192723f52d458d91b40dc9e
k2works <kakimomokuri@gmail.com>
2017-05-25 19:41:20 +0900

    7-2 サイドバーのある画面を作ろう #9
    ・ページ新規作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 # == Route Map
 #
-#                                         Prefix Verb URI Pattern                                         Controller#Action
-#                      html_css_book_index_index GET  /html_css_book/index(.:format)                      html_css_book/index#index
-#                   html_css_book_campaign_index GET  /html_css_book/campaign(.:format)                   html_css_book/campaign#index
-#              react_beginners_guide_excel_index GET  /react_beginners_guide/excel(.:format)              react_beginners_guide/excel#index
-# discovery_react_beginners_guide_whinepad_index GET  /react_beginners_guide/whinepad/discovery(.:format) react_beginners_guide/whinepad#discovery
-#           react_beginners_guide_whinepad_index GET  /react_beginners_guide/whinepad(.:format)           react_beginners_guide/whinepad#index
-#           intro_to_reacts_intro_to_react_index GET  /intro_to_reacts/intro_to_react(.:format)           intro_to_reacts/intro_to_react#index
-#                     beer_lists_beer_list_index GET  /beer_lists/beer_list(.:format)                     beer_lists/beer_list#index
+#                                         Prefix Verb URI Pattern                                           Controller#Action
+#                      html_css_book_index_index GET  /html_css_book/index(.:format)                        html_css_book/index#index
+#                  html_css_book_campaign_submit GET  /html_css_book/campaign/:campaign_id/submit(.:format) html_css_book/campaign#submit
+#                   html_css_book_campaign_index GET  /html_css_book/campaign(.:format)                     html_css_book/campaign#index
+#                    html_css_book_article_index GET  /html_css_book/article(.:format)                      html_css_book/article#index
+#              react_beginners_guide_excel_index GET  /react_beginners_guide/excel(.:format)                react_beginners_guide/excel#index
+# discovery_react_beginners_guide_whinepad_index GET  /react_beginners_guide/whinepad/discovery(.:format)   react_beginners_guide/whinepad#discovery
+#           react_beginners_guide_whinepad_index GET  /react_beginners_guide/whinepad(.:format)             react_beginners_guide/whinepad#index
+#           intro_to_reacts_intro_to_react_index GET  /intro_to_reacts/intro_to_react(.:format)             intro_to_reacts/intro_to_react#index
+#                     beer_lists_beer_list_index GET  /beer_lists/beer_list(.:format)                       beer_lists/beer_list#index
 #
 
 Rails.application.routes.draw do
@@ -16,6 +18,7 @@ Rails.application.routes.draw do
     resources :campaign, only: [:index] do
       get 'submit', to: 'campaign#submit'
     end
+    resources :article, only: [:index]
   end
 
   namespace :react_beginners_guide do
