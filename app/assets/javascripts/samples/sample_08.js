eb48848637f3a9c8db559d5f02940fa8571e39b7
k2works <kakimomokuri@gmail.com>
2017-05-26 19:22:56 +0900

    画像、音声、動画の利用 #14
    ・画像を２値化する

--- app/assets/javascripts/samples/sample_08.js
+++ app/assets/javascripts/samples/sample_08.js
@@ -53,3 +53,59 @@ $(function() {
         tgt.empty().append(canvas);
     }
 });
+
+// DOMの準備後に実行する内容
+$(function() {
+    // ［実行］ボタンを押した時の処理を設定
+    $("#execButton").click(function() {
+        // 変数の初期化
+        var img = $("#source");
+        var w = img.width();
+        var h = img.height();
+
+        // Canvasを作成
+        var canvas = $("<canvas>")
+            .attr("width", w).attr("height", h).get(0);
+        if (! canvas || ! canvas.getContext) return;
+        var context = canvas.getContext("2d");
+
+        // 画像の描画
+        context.drawImage(img.get(0), 0, 0, w, h);
+
+        // 画素の取得
+        var imagedata = context.getImageData(0, 0, w, h);
+        var data = imagedata.data;
+
+        // 画素の加工
+        for (var y = 0; y < h; y ++) {
+            for (var x = 0; x < w; x ++) {
+                // 画素の位置を計算
+                var pos = (x + y * w) * 4;
+
+                // RGBAの値を取り出し
+                var r = data[pos + 0];
+                var g = data[pos + 1];
+                var b = data[pos + 2];
+                var a = data[pos + 3];
+
+                // RGBの値の平均値を計算
+                var average = (r + g + b) / 3;
+
+                // 平均が128未満か否かで、白黒を分別
+                var bin = 255;
+                if (average < 128) bin = 0;
+
+                // RGBの値を上書き
+                data[pos + 0] = bin;
+                data[pos + 1] = bin;
+                data[pos + 2] = bin;
+            }
+        }
+
+        // 画素の反映
+        context.putImageData(imagedata, 0, 0);
+
+        // 表示
+        $("#output").append(canvas);
+    });
+});
