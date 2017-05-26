a60a166d00f946cfd48b53223dc7614e6cf54768
k2works <kakimomokuri@gmail.com>
2017-05-26 16:01:06 +0900

    Bootstrap3におけるJavaScriptのプログミングモデル #12
    ・スクロールと連動したナビゲーション

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@
 #                          bootstraps_form_index GET  /bootstraps/form(.:format)                            bootstraps/form#index
 #                            bootstraps_ui_index GET  /bootstraps/ui(.:format)                              bootstraps/ui#index
 #                   bootstraps_java_script_index GET  /bootstraps/java_script(.:format)                     bootstraps/java_script#index
+#                         bootstraps_affix_index GET  /bootstraps/affix(.:format)                           bootstraps/affix#index
 #                      html_css_book_index_index GET  /html_css_book/index(.:format)                        html_css_book/index#index
 #                  html_css_book_campaign_submit GET  /html_css_book/campaign/:campaign_id/submit(.:format) html_css_book/campaign#submit
 #                   html_css_book_campaign_index GET  /html_css_book/campaign(.:format)                     html_css_book/campaign#index
@@ -27,6 +28,7 @@ Rails.application.routes.draw do
     resources :form, only: [:index]
     resources :ui, only: [:index]
     resources :java_script, only: [:index]
+    resources :affix, only: [:index]
   end
 
   namespace :html_css_book do
