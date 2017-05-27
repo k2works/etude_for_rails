e6bd7d0c5a86ff6ad2a8e6407fb813d6afa5852c
k2works <kakimomokuri@gmail.com>
2017-05-27 17:59:18 +0900

    スクリプト更新 #15
    ・ブラウザをリロードしないとJavaScriptが動かない対応

--- app/assets/javascripts/samples/sample_06.js
+++ app/assets/javascripts/samples/sample_06.js
@@ -1,89 +1,93 @@
-$(function () {
-    // オブジェクトを作成
-    var obj = {
-        name: "りんご"
-        ,price: 100
-        ,color: "赤"
-    };
+$(document).on("turbolinks:load", function() {
+    (function(){
+        $(function () {
+            // オブジェクトを作成
+            var obj = {
+                name: "りんご"
+                ,price: 100
+                ,color: "赤"
+            };
 
-    // 文字列を出力1
-    $("#output21").append("文字列を出力1\n");
-    $("#output21").append("obj.name : " + obj.name + "\n");
-    $("#output21").append("obj.price : " + obj.price + "\n");
-    $("#output21").append("obj.color : " + obj.color + "\n");
-    $("#output21").append("<hr>");
+            // 文字列を出力1
+            $("#output21").append("文字列を出力1\n");
+            $("#output21").append("obj.name : " + obj.name + "\n");
+            $("#output21").append("obj.price : " + obj.price + "\n");
+            $("#output21").append("obj.color : " + obj.color + "\n");
+            $("#output21").append("<hr>");
 
-    // 文字列を出力2
-    $("#output21").append("文字列を出力2\n");
-    $("#output21").append("obj.name : " + obj["name"] + "\n");
-    $("#output21").append("obj.price : " + obj["price"] + "\n");
-    $("#output21").append("obj.color : " + obj["color"] + "\n");
-    $("#output21").append("<hr>");
+            // 文字列を出力2
+            $("#output21").append("文字列を出力2\n");
+            $("#output21").append("obj.name : " + obj["name"] + "\n");
+            $("#output21").append("obj.price : " + obj["price"] + "\n");
+            $("#output21").append("obj.color : " + obj["color"] + "\n");
+            $("#output21").append("<hr>");
 
-    // 値の書き換え
-    obj.name = "apple";
-    obj["price"] = 200;
+            // 値の書き換え
+            obj.name = "apple";
+            obj["price"] = 200;
 
-    $("#output21").append("値の書き換え\n");
-    $("#output21").append("obj.name : " + obj.name + "\n");
-    $("#output21").append("obj.price : " + obj.price + "\n");
-    $("#output21").append("<hr>");
+            $("#output21").append("値の書き換え\n");
+            $("#output21").append("obj.name : " + obj.name + "\n");
+            $("#output21").append("obj.price : " + obj.price + "\n");
+            $("#output21").append("<hr>");
 
-    // プロパティの追加
-    obj.taste = "甘酸っぱい";
+            // プロパティの追加
+            obj.taste = "甘酸っぱい";
 
-    $("#output21").append("プロパティの追加\n");
-    $("#output21").append("obj.taste : " + obj.taste + "\n");
-    $("#output21").append("<hr>");
+            $("#output21").append("プロパティの追加\n");
+            $("#output21").append("obj.taste : " + obj.taste + "\n");
+            $("#output21").append("<hr>");
 
-    // 空のオブジェクトを作成
-    var obj2 = {};
+            // 空のオブジェクトを作成
+            var obj2 = {};
 
-    // オブジェクトに様々値を追加
-    obj2.myFnc = function () { return "fnc"};
-    obj2.myArr = [10, 11, 12];
-    obj2.myObj = {name: "hogehoge", price: 99};
+            // オブジェクトに様々値を追加
+            obj2.myFnc = function () { return "fnc"};
+            obj2.myArr = [10, 11, 12];
+            obj2.myObj = {name: "hogehoge", price: 99};
 
-    $("#output21").append("オブジェクトに様々な値を追加\n");
-    $("#output21").append("obj2.myFnc : " + obj2.myFnc() + "\n");
-    $("#output21").append("obj2.myArr : " + obj2.myArr + "\n");
-    $("#output21").append("obj2.myObj : " + obj2.myObj + "\n");
-})
+            $("#output21").append("オブジェクトに様々な値を追加\n");
+            $("#output21").append("obj2.myFnc : " + obj2.myFnc() + "\n");
+            $("#output21").append("obj2.myArr : " + obj2.myArr + "\n");
+            $("#output21").append("obj2.myObj : " + obj2.myObj + "\n");
+        })
 
-$(function () {
-    // windowオブジェクトの中身を出力
-    for (var key in window) {
-        $("#output22").append(key + " : " + window[key] + "\n");
-    }
-});
+        $(function () {
+            // windowオブジェクトの中身を出力
+            for (var key in window) {
+                $("#output22").append(key + " : " + window[key] + "\n");
+            }
+        });
 
-$(function () {
-    // 関数オブジェクトを作成
-    var fruit = function(name, price) {
-        // 外部からアクセス可能なプロパティ
-        this.name = name;
-        this.price = price;
+        $(function () {
+            // 関数オブジェクトを作成
+            var fruit = function(name, price) {
+                // 外部からアクセス可能なプロパティ
+                this.name = name;
+                this.price = price;
 
-        // 外部からアクセスできない変数
-        var type = "fruit";
+                // 外部からアクセスできない変数
+                var type = "fruit";
 
-        // 外部からアクセス可能なメソッド
-        this.toString = function () {
-            return this.name + ", " + this.price + ", " + type;
-        };
-    };
+                // 外部からアクセス可能なメソッド
+                this.toString = function () {
+                    return this.name + ", " + this.price + ", " + type;
+                };
+            };
 
-    // new演算子でオブジェクトを作る
-    var apple = new fruit("りんご", 100);
-    var banana = new fruit("ばなな", 90);
+            // new演算子でオブジェクトを作る
+            var apple = new fruit("りんご", 100);
+            var banana = new fruit("ばなな", 90);
 
-    // 文字列を出力
-    $("#output23").append("apple.name : " + apple.name + "\n");
-    $("#output23").append("apple.price : " + apple.price + "\n");
-    $("#output23").append("apple.toString : " + apple.toString() + "\n");
-    $("#output23").append("<hr>");
+            // 文字列を出力
+            $("#output23").append("apple.name : " + apple.name + "\n");
+            $("#output23").append("apple.price : " + apple.price + "\n");
+            $("#output23").append("apple.toString : " + apple.toString() + "\n");
+            $("#output23").append("<hr>");
 
-    $("#output23").append("banana.name : " + banana.name + "\n");
-    $("#output23").append("banana.price : " + banana.price + "\n");
-    $("#output23").append("banana.toString : " + banana.toString() + "\n");
+            $("#output23").append("banana.name : " + banana.name + "\n");
+            $("#output23").append("banana.price : " + banana.price + "\n");
+            $("#output23").append("banana.toString : " + banana.toString() + "\n");
+        });
+    }());
 });
--- app/assets/javascripts/samples/sample_08.js
+++ app/assets/javascripts/samples/sample_08.js
@@ -1,124 +1,128 @@
-// DOMの準備後に実行する内容
-$(function() {
-    // 画像ファイル名を2つ取得
-    var tgt = $("#checkered");		// id「checkered」のdiv要素を選択
-    var text = tgt.text();			// 内部の文字列を取り出し
-    var imgs = text.split("|");		// 画像URLの配列を作る
-    if (imgs.length != 2) return;	// 要素数が「2」でなければ終了
+$(document).on("turbolinks:load", function() {
+    (function(){
+        // DOMの準備後に実行する内容
+        $(function() {
+            // 画像ファイル名を2つ取得
+            var tgt = $("#checkered");		// id「checkered」のdiv要素を選択
+            var text = tgt.text();			// 内部の文字列を取り出し
+            var imgs = text.split("|");		// 画像URLの配列を作る
+            if (imgs.length != 2) return;	// 要素数が「2」でなければ終了
 
-    // 画像用の変数を初期化
-    var count = 0;		// 画像読み込み終了を確認するための変数
-    var imgObj = [];	// 画像格納用の配列
+            // 画像用の変数を初期化
+            var count = 0;		// 画像読み込み終了を確認するための変数
+            var imgObj = [];	// 画像格納用の配列
 
-    // 画像を読み込み
-    for (var i = 0; i < imgs.length; i ++) {
-        imgObj[i] = new Image();		// Imageオブジェクトの作成
-        imgObj[i].onload = finishLoad;	// 読み込み完了時の処理を登録
-        imgObj[i].src = imgs[i];		// 画像のURLを登録
-    }
-
-    // 読み込み完了時の処理
-    function finishLoad() {
-        // 全ての画像が読み込み終わりか確認
-        count ++;
-        if (count != imgs.length) return;
-
-        // 全ての画像が読み込み終わったのでCanvasを作成して描画
-
-        // 変数の初期化
-        var w = imgObj[0].width;
-        var h = imgObj[0].height;
-        var u = 64;		// 描画単位（市松模様のサイズ）
-
-        // Canvasを作成して、2Dコンテクストを取り出す
-        var canvas = $("<canvas>")
-            .attr("width", w).attr("height", h).get(0);
-        if (! canvas || ! canvas.getContext) return;
-        var context = canvas.getContext("2d");
-
-        // 画像の描画
-        for (var y = 0; y < h / u; y ++) {
-            for (var x = 0; x < w / u; x ++) {
-                // 描画する画像を決定して、配列から取り出す
-                var img = imgObj[(y % 2 + x) % 2];
-
-                // 画像を2Dコンテクストに描画
-                context.drawImage(img,
-                    x * u, y * u, u, u,
-                    x * u, y * u, u, u);
+            // 画像を読み込み
+            for (var i = 0; i < imgs.length; i ++) {
+                imgObj[i] = new Image();		// Imageオブジェクトの作成
+                imgObj[i].onload = finishLoad;	// 読み込み完了時の処理を登録
+                imgObj[i].src = imgs[i];		// 画像のURLを登録
             }
-        }
 
-        // 表示
-        tgt.empty().append(canvas);
-    }
-});
+            // 読み込み完了時の処理
+            function finishLoad() {
+                // 全ての画像が読み込み終わりか確認
+                count ++;
+                if (count != imgs.length) return;
 
-// DOMの準備後に実行する内容
-$(function() {
-    // ［実行］ボタンを押した時の処理を設定
-    $("#execButton").click(function() {
-        // 変数の初期化
-        var img = $("#source");
-        var w = img.width();
-        var h = img.height();
+                // 全ての画像が読み込み終わったのでCanvasを作成して描画
 
-        // Canvasを作成
-        var canvas = $("<canvas>")
-            .attr("width", w).attr("height", h).get(0);
-        if (! canvas || ! canvas.getContext) return;
-        var context = canvas.getContext("2d");
+                // 変数の初期化
+                var w = imgObj[0].width;
+                var h = imgObj[0].height;
+                var u = 64;		// 描画単位（市松模様のサイズ）
 
-        // 画像の描画
-        context.drawImage(img.get(0), 0, 0, w, h);
+                // Canvasを作成して、2Dコンテクストを取り出す
+                var canvas = $("<canvas>")
+                    .attr("width", w).attr("height", h).get(0);
+                if (! canvas || ! canvas.getContext) return;
+                var context = canvas.getContext("2d");
 
-        // 画素の取得
-        var imagedata = context.getImageData(0, 0, w, h);
-        var data = imagedata.data;
+                // 画像の描画
+                for (var y = 0; y < h / u; y ++) {
+                    for (var x = 0; x < w / u; x ++) {
+                        // 描画する画像を決定して、配列から取り出す
+                        var img = imgObj[(y % 2 + x) % 2];
 
-        // 画素の加工
-        for (var y = 0; y < h; y ++) {
-            for (var x = 0; x < w; x ++) {
-                // 画素の位置を計算
-                var pos = (x + y * w) * 4;
+                        // 画像を2Dコンテクストに描画
+                        context.drawImage(img,
+                            x * u, y * u, u, u,
+                            x * u, y * u, u, u);
+                    }
+                }
 
-                // RGBAの値を取り出し
-                var r = data[pos + 0];
-                var g = data[pos + 1];
-                var b = data[pos + 2];
-                var a = data[pos + 3];
-
-                // RGBの値の平均値を計算
-                var average = (r + g + b) / 3;
-
-                // 平均が128未満か否かで、白黒を分別
-                var bin = 255;
-                if (average < 128) bin = 0;
-
-                // RGBの値を上書き
-                data[pos + 0] = bin;
-                data[pos + 1] = bin;
-                data[pos + 2] = bin;
+                // 表示
+                tgt.empty().append(canvas);
             }
-        }
+        });
 
-        // 画素の反映
-        context.putImageData(imagedata, 0, 0);
+        // DOMの準備後に実行する内容
+        $(function() {
+            // ［実行］ボタンを押した時の処理を設定
+            $("#execButton").click(function() {
+                // 変数の初期化
+                var img = $("#source");
+                var w = img.width();
+                var h = img.height();
 
-        // 表示
-        $("#output").append(canvas);
-    });
-});
+                // Canvasを作成
+                var canvas = $("<canvas>")
+                    .attr("width", w).attr("height", h).get(0);
+                if (! canvas || ! canvas.getContext) return;
+                var context = canvas.getContext("2d");
 
-// DOMの準備後に実行する内容
-$(function() {
-    // ［再生］ボタンを押した時の処理を設定
-    $("#playButton").click(function() {
-        $("#video").get(0).play();
-    });
+                // 画像の描画
+                context.drawImage(img.get(0), 0, 0, w, h);
 
-    // ［停止］ボタンを押した時の処理を設定
-    $("#pauseButton").click(function() {
-        $("#video").get(0).pause();
-    });
+                // 画素の取得
+                var imagedata = context.getImageData(0, 0, w, h);
+                var data = imagedata.data;
+
+                // 画素の加工
+                for (var y = 0; y < h; y ++) {
+                    for (var x = 0; x < w; x ++) {
+                        // 画素の位置を計算
+                        var pos = (x + y * w) * 4;
+
+                        // RGBAの値を取り出し
+                        var r = data[pos + 0];
+                        var g = data[pos + 1];
+                        var b = data[pos + 2];
+                        var a = data[pos + 3];
+
+                        // RGBの値の平均値を計算
+                        var average = (r + g + b) / 3;
+
+                        // 平均が128未満か否かで、白黒を分別
+                        var bin = 255;
+                        if (average < 128) bin = 0;
+
+                        // RGBの値を上書き
+                        data[pos + 0] = bin;
+                        data[pos + 1] = bin;
+                        data[pos + 2] = bin;
+                    }
+                }
+
+                // 画素の反映
+                context.putImageData(imagedata, 0, 0);
+
+                // 表示
+                $("#output").append(canvas);
+            });
+        });
+
+        // DOMの準備後に実行する内容
+        $(function() {
+            // ［再生］ボタンを押した時の処理を設定
+            $("#playButton").click(function() {
+                $("#video").get(0).play();
+            });
+
+            // ［停止］ボタンを押した時の処理を設定
+            $("#pauseButton").click(function() {
+                $("#video").get(0).pause();
+            });
+        });
+    }());
 });
