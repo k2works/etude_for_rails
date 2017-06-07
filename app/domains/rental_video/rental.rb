c5062dca9d48fb97711e2c4c2281b87535b747ea
k2works <kakimomokuri@gmail.com>
2017-06-07 12:37:48 +0900

    最初の状態 #21

---
+++ app/domains/rental_video/rental.rb
@@ -0,0 +1,9 @@
+module RentalVideo
+  class Rental
+    attr_reader :movie, :days_rented
+
+    def initialize(movie, days_rented)
+      @movie, @days_rented = movie, days_rented
+    end
+  end
+end
---
+++ spec/domains/rental_video/customer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+include RentalVideo
+
+describe RentalVideo::Customer do
+  describe '#add_rental' do
+    it 'will do something'
+  end
+
+  describe '#statement' do
+    it 'will do something'
+  end
+end
