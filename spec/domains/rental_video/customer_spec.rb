a52d65e203e9406dd205c68d800565efea24b588
k2works <kakimomokuri@gmail.com>
2017-06-08 11:01:13 +0900

    リファクタリング #21
    メソッドの抽出を実施する。

--- spec/domains/rental_video/customer_spec.rb
+++ spec/domains/rental_video/customer_spec.rb
@@ -1,30 +1,28 @@
 require 'rails_helper'
 include RentalVideo
 
+def rental_movie(days_rented, price_code)
+  mv = RentalVideo::Movie.new('Attack of the Killer Tomatoes!', price_code)
+  RentalVideo::Rental.new(mv, days_rented)
+end
+
 describe RentalVideo::Customer do
   let(:customer) {Customer.new('Taro')}
   describe '#add_rental' do
-    it 'shuld be rented' do
-      mv = RentalVideo::Movie.new('Attack of the Killer Tomatoes!', Movie::REGULAR)
-      rental = RentalVideo::Rental.new(mv, 7)
-      customer.add_rental(rental)
+    it 'should be rented' do
+      customer.add_rental(rental_movie(7, RentalVideo::Movie::REGULAR))
       expect(customer.instance_variable_get :@rentals).not_to be_empty
     end
   end
 
   describe '#statement' do
     it 'calculate price' do
-      mv = RentalVideo::Movie.new('Attack of the Killer Tomatoes!', Movie::REGULAR)
-      rental = RentalVideo::Rental.new(mv, 7)
-      customer.add_rental(rental)
+      customer.add_rental(rental_movie(7,RentalVideo::Movie::REGULAR))
       expect(customer.statement).to be_include '9.5'
-      rental
     end
 
     it 'print statement' do
-      mv = RentalVideo::Movie.new('Attack of the Killer Tomatoes!', Movie::REGULAR)
-      rental = RentalVideo::Rental.new(mv, 7)
-      customer.add_rental(rental)
+      customer.add_rental(rental_movie(7,RentalVideo::Movie::REGULAR))
       expect(customer.statement).to be_match 'Rental Record for Taro\n\tAttack of the Killer Tomatoes!\t9.5\nAmount owed is 0.0\nYou earned 1 frequent renter points'
     end
   end
