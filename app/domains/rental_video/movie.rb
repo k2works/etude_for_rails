7e8d012f64b7846ce3d568cd59b0e13d553e164e
k2works <kakimomokuri@gmail.com>
2017-06-08 13:56:23 +0900

    料金コードによる条件分岐からポリモーフィズムへ #21

--- app/domains/rental_video/movie.rb
+++ app/domains/rental_video/movie.rb
@@ -10,5 +10,24 @@ module RentalVideo
     def initialize(title, price_code)
       @title, @price_code = title, price_code
     end
+
+    def charge(days_rented)
+      result = 0
+      case price_code
+        when Movie::REGULAR
+          result += 2
+          result += (days_rented - 2) * 1.5 if days_rented > 2
+        when Movie::NEW_RELEASE
+          result += days_rented * 3
+        when Movie::CHILDREN
+          result += 1.5
+          result += (days_rented - 3) * 1.5 if days_rented > 3
+      end
+      result
+    end
+
+    def frequent_renter_points(days_rented)
+      (price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+    end
   end
 end
