c97e39b5f042c5923f94ad6174bd986f221be02e
k2works <kakimomokuri@gmail.com>
2017-06-08 10:56:47 +0900

    リファクタリング #21
    一時変数のインライン化を実施する。

--- spec/domains/rental_video/customer_spec.rb
+++ spec/domains/rental_video/customer_spec.rb
@@ -2,32 +2,30 @@ require 'rails_helper'
 include RentalVideo
 
 describe RentalVideo::Customer do
+  let(:customer) {Customer.new('Taro')}
   describe '#add_rental' do
     it 'shuld be rented' do
-      cs = Customer.new('Taro')
       mv = RentalVideo::Movie.new('Attack of the Killer Tomatoes!', Movie::REGULAR)
       rental = RentalVideo::Rental.new(mv, 7)
-      cs.add_rental(rental)
-      expect(cs.instance_variable_get :@rentals).not_to be_empty
+      customer.add_rental(rental)
+      expect(customer.instance_variable_get :@rentals).not_to be_empty
     end
   end
 
   describe '#statement' do
     it 'calculate price' do
-      cs = Customer.new('Taro')
       mv = RentalVideo::Movie.new('Attack of the Killer Tomatoes!', Movie::REGULAR)
       rental = RentalVideo::Rental.new(mv, 7)
-      cs.add_rental(rental)
-      expect(cs.statement).to be_include '9.5'
+      customer.add_rental(rental)
+      expect(customer.statement).to be_include '9.5'
       rental
     end
 
     it 'print statement' do
-      cs = Customer.new('Taro')
       mv = RentalVideo::Movie.new('Attack of the Killer Tomatoes!', Movie::REGULAR)
       rental = RentalVideo::Rental.new(mv, 7)
-      cs.add_rental(rental)
-      expect(cs.statement).to be_match 'Rental Record for Taro\n\tAttack of the Killer Tomatoes!\t9.5\nAmount owed is 0.0\nYou earned 1 frequent renter points'
+      customer.add_rental(rental)
+      expect(customer.statement).to be_match 'Rental Record for Taro\n\tAttack of the Killer Tomatoes!\t9.5\nAmount owed is 0.0\nYou earned 1 frequent renter points'
     end
   end
 end
