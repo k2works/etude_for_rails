237e55d11f80483f42b690ed491efcb8f81ec65d
k2works <kakimomokuri@gmail.com>
2017-06-12 11:51:20 +0900

    時間給の従業員の給与 #23
    ・TestPaySingleHourlyEmployeeOnWrongDate

--- spec/domains/payroll/payroll_spec.rb
+++ spec/domains/payroll/payroll_spec.rb
@@ -440,5 +440,18 @@ describe Payroll::PaydayTransaction do
       pt.execute
       validate_paycheck(emp_id, (8 + 1.5) * 15.25, pay_date, pt)
     end
+
+    it 'create pay check for single hourly employee on wrong date' do
+      emp_id = 2
+      t = Payroll::AddHourlyEmployee.new(emp_id, 'Bill', 'Home', 15.25)
+      t.execute
+      pay_date = Date.new(2001,11,8)
+      tc = TimeCardTransaction.new(pay_date, 9.0, emp_id)
+      tc.execute
+      pt = Payroll::PaydayTransaction.new(pay_date)
+      pt.execute
+      pc = pt.get_paycheck(emp_id)
+      expect(pc).to be_nil
+    end
   end
 end
