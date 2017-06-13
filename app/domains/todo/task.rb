65c26789cc1cb951cf4284fb19703b41e5521f90
k2works <kakimomokuri@gmail.com>
2017-06-13 18:10:47 +0900

    15-4 データベースアクセス処理とCommandクラス #24
    ・モデルクラスの作成
    
    EFROR-2

---
+++ app/domains/todo/task.rb
@@ -0,0 +1,19 @@
+module Todo
+  # taskテーブルを表現するモデルクラスです
+  # @author k2works
+  class Task < ActiveRecord::Base
+
+    NOT_YET = 0 # タスクが完了していない
+
+    DONE = 1 # タスクが完了した
+
+    PENDING = 2 # 保留中のタスク
+
+    # ステータスの名称と数値の組み合わせの値
+    STATUS = {
+        'NOT_YET' => NOT_YET,
+        'DONE' => DONE,
+        'PENDING' => PENDING
+    }.freeze
+  end
+end
