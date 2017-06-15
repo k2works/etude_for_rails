61c6bd252d9e00f31c649b8c8527ab498f8ad3cd
k2works <kakimomokuri@gmail.com>
2017-06-15 15:32:08 +0900

    15-5 コマンドラインからの引数解析 #24
    ・コマンドライン引数をCommandクラスに渡す
    
    EFROR-2

--- app/domains/todo/command.rb
+++ app/domains/todo/command.rb
@@ -3,7 +3,17 @@ module Todo
   # @author k2works
   class Command
 
+    def self.run(argv)
+      new(argv).execute
+    end
+
+    def initialize(argv)
+      @argv = argv
+    end
+
     def execute
+      options = Options.parse!(@argv)
+
       DB.prepare
     end
 
