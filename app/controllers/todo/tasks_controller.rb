9cdc9903101b4d4a0497fa42749a3b64806d57b3
k2works <kakimomokuri@gmail.com>
2017-06-16 17:00:43 +0900

    16-3 Webインターフェースの追加 #25
    ・Todo1.0.0との結合
    
    EFROR-5

---
+++ app/controllers/todo/tasks_controller.rb
@@ -0,0 +1,79 @@
+class Todo::TasksController < ApplicationController
+  before_action :set_todo_task, only: [:show, :edit, :update, :destroy]
+  before_action :prepare
+
+  # GET /todo/tasks
+  # GET /todo/tasks.json
+  def index
+    @todo_tasks = Todo::Task.all
+  end
+
+  # GET /todo/tasks/1
+  # GET /todo/tasks/1.json
+  def show
+  end
+
+  # GET /todo/tasks/new
+  def new
+    @todo_task = Todo::Task.new
+  end
+
+  # GET /todo/tasks/1/edit
+  def edit
+  end
+
+  # POST /todo/tasks
+  # POST /todo/tasks.json
+  def create
+    @todo_task = Todo::Task.new(todo_task_params)
+
+    respond_to do |format|
+      if @todo_task.save
+        format.html { redirect_to @todo_task, notice: 'Task was successfully created.' }
+        format.json { render :show, status: :created, location: @todo_task }
+      else
+        format.html { render :new }
+        format.json { render json: @todo_task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /todo/tasks/1
+  # PATCH/PUT /todo/tasks/1.json
+  def update
+    respond_to do |format|
+      if @todo_task.update(todo_task_params)
+        format.html { redirect_to @todo_task, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @todo_task }
+      else
+        format.html { render :edit }
+        format.json { render json: @todo_task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /todo/tasks/1
+  # DELETE /todo/tasks/1.json
+  def destroy
+    @todo_task.destroy
+    respond_to do |format|
+      format.html { redirect_to todo_tasks_url, notice: 'Task was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_todo_task
+    @todo_task = Todo::Task.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def todo_task_params
+    params.require(:todo_task).permit(:name, :content, :status)
+  end
+
+  def prepare
+    Todo::DB.prepare
+  end
+end
