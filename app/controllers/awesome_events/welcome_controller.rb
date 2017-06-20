97d4536d379e4959f67d317ff47c51bf897b090b
k2works <kakimomokuri@gmail.com>
2017-06-20 17:48:24 +0900

    6-5 イベントの閲覧機能を作る #28 #29
    ・イベント一覧ページの作成
    
    EFROR-8

--- app/controllers/awesome_events/welcome_controller.rb
+++ app/controllers/awesome_events/welcome_controller.rb
@@ -1,4 +1,5 @@
 class AwesomeEvents::WelcomeController < AwesomeEvents::ApplicationController
   def index
+    @events = AwesomeEvents::Event.where('start_time > ?', Time.zone.now).order(:start_time)
   end
 end
