81071055d6b47876ef28c923c9f9670fe997912e
k2works <kakimomokuri@gmail.com>
2017-06-27 14:56:49 +0900

    6-8 退会機能を作る #34
    ・退会処理の作成
    
    EFROR-13

--- app/controllers/awesome_events/users_controller.rb
+++ app/controllers/awesome_events/users_controller.rb
@@ -4,4 +4,12 @@ class AwesomeEvents::UsersController < AwesomeEvents::ApplicationController
   def retire
   end
 
+  def destroy
+    if current_user.destroy
+      reset_session
+      redirect_to awesome_events_welcome_index_path, notice: '退会完了しました'
+    else
+      render :retire
+    end
+  end
 end
--- app/models/awesome_events/user.rb
+++ app/models/awesome_events/user.rb
@@ -12,8 +12,11 @@
 #
 
 class AwesomeEvents::User < ApplicationRecord
-  has_many :created_events, class_name: 'AwesomeEvents::Event', foreign_key: :owner_id
-  has_many :tickets, class_name: 'AwesomeEvents::Ticket', foreign_key: :awesome_events_user_id
+  before_destroy :check_all_events_finished
+
+  has_many :created_events, class_name: 'AwesomeEvents::Event', foreign_key: :owner_id, dependent: :nullify
+  has_many :tickets, class_name: 'AwesomeEvents::Ticket', foreign_key: :awesome_events_user_id, dependent: :nullify
+  has_many :participating_events, through: :tickets, source: :event
 
   def self.find_or_create_from_auth_hash(auth_hash)
     provider = auth_hash[:provider]
@@ -25,5 +28,19 @@ class AwesomeEvents::User < ApplicationRecord
       user.nickname = nickname
       user.image_url = image_url
     end
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(':now < end_time', now: now).exists?
+      errors[:base] << '公開中の未終了イベントが存在します。'
+    end
+
+    if participating_events.where(':now < end_time', now: now).exists?
+      errors[:base] << '未終了の参加イベントが存在します。'
+    end
+    throw(:abort) unless errors.blank?
   end
 end
