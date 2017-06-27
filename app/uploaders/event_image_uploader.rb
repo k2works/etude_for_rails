85474a18d6e734037fe453ffe3169391085f06d5
k2works <kakimomokuri@gmail.com>
2017-06-27 18:06:12 +0900

    6-10 gemで機能拡張をする #36
    ・アップロードにおけるその他の注意点
    
    EFROR-33

--- app/uploaders/event_image_uploader.rb
+++ app/uploaders/event_image_uploader.rb
@@ -2,7 +2,7 @@ class EventImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -28,6 +28,7 @@ class EventImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  process :resize_to_limit => [1130, 1130]
 
   # Create different versions of your uploaded files:
   # version :thumb do
@@ -36,9 +37,9 @@ class EventImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
