4d8c1d4220a64c38cff79e1782da10353cc0de05
k2works <kakimomokuri@gmail.com>
2017-06-29 15:18:33 +0900

    7-7 エンドツーエンドのテスト #28 #38
    ・エンドツーエンドのテストを書く
    
    EFROR-34

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,17 @@ SimpleCov.start
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
+  config.before(:all, type: :feature) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+        provider: 'twitter',
+        uid: '12345',
+        info: {
+            nickname: 'netwillnet',
+            image: 'http://example.com/netwillnet.jpg'
+        }})
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
