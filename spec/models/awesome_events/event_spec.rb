4688d0b554f7baae824a4ef90add97ac50541211
k2works <kakimomokuri@gmail.com>
2017-06-29 12:13:42 +0900

    7-4 モデルのテスト  #28 #38
    ・shouldaを利用してバリデーションのテストを書く（導入前）
    
    EFROR-34

--- spec/models/awesome_events/event_spec.rb
+++ spec/models/awesome_events/event_spec.rb
@@ -2,12 +2,50 @@ require 'rails_helper'
 
 RSpec.describe AwesomeEvents::Event, type: :model do
   describe '#name' do
+    context '#name' do
+      context 'nilのとき' do
+        let(:event) { AwesomeEvents::Event.new(name: nil) }
+
+        it 'validでないこと' do
+          event.valid?
+          expect(event.errors[:name]).to be_present
+        end
+      end
+    end
+
     context '空白のとき' do
       let(:event) { AwesomeEvents::Event.new(name: '') }
 
       it 'valida でないこと' do
         event.valid?
         expect(event.errors[:name]).to be_present
+      end
+    end
+
+    context 'Rails勉強会の時' do
+      let(:event) { AwesomeEvents::Event.new(name: 'Rails勉強会') }
+
+      it 'validであること' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+    end
+
+    context '50文字のとき' do
+      let(:event) { AwesomeEvents::Event.new(name: 'a' * 50) }
+
+      it 'validであること' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+
+      context '51文字のとき' do
+        let(:event) { AwesomeEvents::Event.new(name: 'a' * 51) }
+
+        it 'validでないこと' do
+          event.valid?
+          expect(event.errors[:name]).to be_present
+        end
       end
     end
   end
