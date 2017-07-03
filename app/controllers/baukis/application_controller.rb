7e8e0367ce2ba0075e3d02018082e1f33c1c7e8e
k2works <kakimomokuri@gmail.com>
2017-07-03 17:48:40 +0900

    7-5 ActiveSupport::Concern #51
    EFROR-36

--- app/controllers/baukis/application_controller.rb
+++ app/controllers/baukis/application_controller.rb
@@ -5,10 +5,7 @@ class Baukis::ApplicationController <  ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  rescue_from Exception, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
-  rescue_from ActionController::RoutingError, with: :rescue404
+  include ErrorHandlers if Rails.env.production?
 
   private
   def set_layout
@@ -17,20 +14,5 @@ class Baukis::ApplicationController <  ActionController::Base
     else
       'baukis_customer'
     end
-  end
-
-  def rescue403(e)
-    @exception = e
-    render 'baukis/errors/forbidden', status: 403
-  end
-
-  def rescue404(e)
-    @exception = e
-    render 'baukis/errors/not_found', status: 404
-  end
-
-  def rescue500(e)
-    @exception = e
-    render 'baukis/errors/internal_server_error', status: 500
   end
 end
