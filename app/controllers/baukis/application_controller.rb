10b122910ea577762034e9a3bc8796170f0e3fad
k2works <kakimomokuri@gmail.com>
2017-07-03 17:14:27 +0900

    7-3 403 Forbidden #51
    ・例外の捕捉
    
    EFROR-36

--- app/controllers/baukis/application_controller.rb
+++ app/controllers/baukis/application_controller.rb
@@ -2,7 +2,12 @@ class Baukis::ApplicationController <  ActionController::Base
   protect_from_forgery with: :exception
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
   rescue_from Exception, with: :rescue500
+  rescue_from Forbidden, with: :rescure403
+  rescue_from IpAddressRejected, with: :rescue403
 
   private
   def set_layout
@@ -11,6 +16,11 @@ class Baukis::ApplicationController <  ActionController::Base
     else
       'baukis_customer'
     end
+  end
+
+  def rescue403(e)
+    @exception = e
+    render 'baukis/errors/forbidden', status: 403
   end
 
   def rescue500(e)
