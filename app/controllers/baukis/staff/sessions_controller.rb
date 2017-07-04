21882c3a41f0447f230cb929c92c757f591b2e7f
k2works <kakimomokuri@gmail.com>
2017-07-04 14:46:09 +0900

    9-3 ログイン・ログアウト後のメッセージ表示 #41
    ・フラッシュへの書き込み
    
    EFROR-16

--- app/controllers/baukis/staff/sessions_controller.rb
+++ app/controllers/baukis/staff/sessions_controller.rb
@@ -15,14 +15,17 @@ class Baukis::Staff::SessionsController < Baukis::Staff::Base
     end
     if Baukis::Staff::Authenticator.new(staff_member).authenticate(@form.password)
       session[:staff_member_id] = staff_member.id
+      flash.notice = 'ログインしました。'
       redirect_to :baukis_staff_root
     else
+      flash.now.alert = 'メールアドレスまたはパスワードが正しくありません。'
       render action: 'new'
     end
   end
 
   def destroy
     session.delete(:staff_member_id)
+    flash.notice = 'ログアウトしました。'
     redirect_to :baukis_staff_root
   end
 
