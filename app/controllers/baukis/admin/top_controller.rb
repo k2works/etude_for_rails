9478fbbe757d82ca384e140f23fe22127fca5de6
k2works <kakimomokuri@gmail.com>
2017-07-04 16:06:08 +0900

    10-2 リソースベースのルーティング #51
    ・リンクの設置
    
    EFROR-37

--- app/controllers/baukis/admin/top_controller.rb
+++ app/controllers/baukis/admin/top_controller.rb
@@ -1,5 +1,9 @@
 class Baukis::Admin::TopController < Baukis::Admin::Base
   def index
-    render action: 'index'
+    if current_administrator
+      render action: 'dashboard'
+    else
+      render action: 'index'
+    end
   end
 end
