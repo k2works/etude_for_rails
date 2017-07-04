47fa6ec79d843a2c06e62f90062072d978d96282
k2works <kakimomokuri@gmail.com>
2017-07-04 11:59:06 +0900

    8-2 モデル #41
    ・アプリケーションヘルパーの配置場所が原因でテストが正常に動かなくなったので修正
    
    EFROR-16

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def url_for_twitter(user)
     "https://twitter.com/#{user.nickname}"
   end
+
+  def document_title
+    if @title.present?
+      "#{@title} - Baukis"
+    else
+      'Baukis'
+    end
+  end
 end
--- spec/models/baukis/staff_member_spec.rb
+++ spec/models/baukis/staff_member_spec.rb
@@ -20,5 +20,5 @@
 require 'rails_helper'
 
 RSpec.describe Baukis::StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { Baukis::StaffMember.new }
 end
