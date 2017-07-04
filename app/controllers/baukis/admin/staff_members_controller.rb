5d52608a8a3c4e621a28742c86a9310927eb228a
k2works <kakimomokuri@gmail.com>
2017-07-04 17:18:51 +0900

    11-1 管理者による職員アカウント管理機能（前編） #49
    ・showアクション
    
    EFROR-21

--- app/controllers/baukis/admin/staff_members_controller.rb
+++ app/controllers/baukis/admin/staff_members_controller.rb
@@ -2,4 +2,9 @@ class Baukis::Admin::StaffMembersController < Baukis::Admin::Base
   def index
     @staff_members = Baukis::StaffMember.order(:family_name_kana, :given_name_kana)
   end
+
+  def show
+    staff_member = Baukis::StaffMember.find(params[:id])
+    redirect_to edit_baukis_admin_staff_member_url(staff_member)
+  end
 end
