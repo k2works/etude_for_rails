480ca70cc20ba3eaeb695f3c66de96c84e8345b5
k2works <kakimomokuri@gmail.com>
2017-07-06 10:12:49 +0900

    12-2 職員自身によるアカウント管理機能 #43
    ・editアクション
    
    EFROR-18

--- app/controllers/baukis/staff/accounts_controller.rb
+++ app/controllers/baukis/staff/accounts_controller.rb
@@ -2,4 +2,8 @@ class Baukis::Staff::AccountsController < Baukis::Staff::Base
   def show
     @staff_member = current_staff_member
   end
+
+  def edit
+    @staff_member = current_staff_member
+  end
 end
