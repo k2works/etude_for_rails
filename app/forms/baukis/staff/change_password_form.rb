14596aef111e4c5dca517c28a2cfd884123a7f14
k2works <kakimomokuri@gmail.com>
2017-07-06 18:14:33 +0900

    15-2 職員が自分のパスワードを変更する機能 #43
    ・バリデーション
    
    EFROR-18

--- app/forms/baukis/staff/change_password_form.rb
+++ app/forms/baukis/staff/change_password_form.rb
@@ -1,13 +1,15 @@
 class Baukis::Staff::ChangePasswordForm
   include ActiveModel::Model
 
-  attr_accessor :object,
-                :current_password,
-                :new_password,
+  attr_accessor :object, :current_password, :new_password,
                 :new_password_confirmation
-
   validates :new_password, presence: true, confirmation: true
 
+  validate do
+    unless Baukis::Staff::Authenticator.new(object).authenticate(current_password)
+      errors.add(:current_password, :wrong)
+    end
+  end
   def save
     if valid?
       object.password = new_password
