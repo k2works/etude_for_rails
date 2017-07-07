67166723dd80e8db6913c4ff10d0d15fac7fffe0
k2works <kakimomokuri@gmail.com>
2017-07-07 09:45:33 +0900

    16-3 フォームプレゼンター #51
    ・FormPresenterクラスの定義
    
    EFROR-57

---
+++ app/presenters/baukis/form_presenter.rb
@@ -0,0 +1,13 @@
+module Baukis
+  class FormPresenter
+    include HtmlBuilder
+
+    attr_reader :form_builder, :view_context
+    delegate :label, :text_field, :password_field, :check_box, :raio_button, :text_area, :object, to: :form_builder
+
+    def initialize(form_builder, view_context)
+      @form_builder = form_builder
+      @view_context = view_context
+    end
+  end
+end
