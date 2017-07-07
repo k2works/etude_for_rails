0f9d78f5e5a1f89c42d8571389c8dc2082197023
k2works <kakimomokuri@gmail.com>
2017-07-07 18:27:57 +0900

    19-1 Capybara #53
    ・顧客アカウント更新機能のテスト
    
    EFROR-24

--- spec/support/baukis/features_spec_helper.rb
+++ spec/support/baukis/features_spec_helper.rb
@@ -2,8 +2,8 @@ module FeaturesSpecHelper
   def login_as_staff_member(staff_memeber, password = 'pw')
     visit baukis_staff_login_path
     within('#login-form') do
-      fill_in 'メールアドレス', with: staff_memeber.email
-      fill_in 'パスワード', with: password
+      fill_in 'baukis_staff_login_form[email]', with: staff_memeber.email
+      fill_in 'baukis_staff_login_form[password]', with: password
       click_button 'ログイン'
     end
   end
