ca7c17c34be44f10f2d6eb350b03e7e543408769
k2works <kakimomokuri@gmail.com>
2017-07-07 10:43:17 +0900

    16-3 フォームプレゼンター #51
    ・さらなる改善
    
    EFROR-57

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  include Baukis::HtmlBuilder
+
   def url_for_twitter(user)
     "https://twitter.com/#{user.nickname}"
   end
--- app/presenters/baukis/staff_member_form_presenter.rb
+++ app/presenters/baukis/staff_member_form_presenter.rb
@@ -8,12 +8,11 @@ module Baukis
 
     def full_name_block(name1, name2, label_text, options = {})
       markup(:div, class: 'input-block') do |m|
-        m << label(name1, label_text, class: options[:required] ? 'required' : nil)
+        m << decorated_label(name1, label_text, options)
         m << text_field(name1, options)
         m << text_field(name2, options)
       end
     end
-
 
     def check_boxes
       markup(:div, class: 'input-block') do |m|
