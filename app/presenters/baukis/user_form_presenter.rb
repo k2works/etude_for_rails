01beb6d17a365b2e4efe9a39e0476165dc4128f0
k2works <kakimomokuri@gmail.com>
2017-07-10 15:12:39 +0900

    20-1 自宅住所と勤務先の任意入力 #53
    ・パスワードフィールド表示オプション修正
    
    EFROR-24

--- app/presenters/baukis/user_form_presenter.rb
+++ app/presenters/baukis/user_form_presenter.rb
@@ -6,7 +6,7 @@ module Baukis
       else
         markup(:div, class: 'input-block') do |m|
           m << decorated_label(name, label_text, options.merge(required: false))
-          m << password_field(name, options.merge(disabled: true))
+          m << password_field(name, options.merge(disabled: true, class: 'disable'))
           m.button('変更する', type: 'button', id: 'enable-password-field')
           m.button('変更しない', type: 'button', id: 'disable-password-field',style: 'display:none')
           m << error_messages_for(name)
