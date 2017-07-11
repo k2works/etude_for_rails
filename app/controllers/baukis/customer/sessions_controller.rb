a7a13da1c4c5ec21589cb5b6e88594e99441e045
k2works <kakimomokuri@gmail.com>
2017-07-11 14:49:12 +0900

    2.1 顧客のログイン・ログアウト機能 #54
    ・2.1.5 動作確認
    
    EFROR-31

--- app/controllers/baukis/customer/sessions_controller.rb
+++ app/controllers/baukis/customer/sessions_controller.rb
@@ -3,7 +3,7 @@ class Baukis::Customer::SessionsController < Baukis::Customer::Base
 
   def new
     if current_customer
-      redirect_to :customer_root
+      redirect_to :baukis_customer_root
     else
       @form = Baukis::Customer::LoginForm.new
       render action: 'new'
@@ -11,19 +11,14 @@ class Baukis::Customer::SessionsController < Baukis::Customer::Base
   end
 
   def create
-    @form = Customer::LoginForm.new(params[:customer_login_form])
+    @form = Baukis::Customer::LoginForm.new(session_params)
     if @form.email.present?
       customer = Baukis::Customer.find_by(email_for_index: @form.email.downcase)
     end
     if Baukis::Customer::Authenticator.new(customer).authenticate(@form.password)
-      if @form.remember_me?
-        cookies.permanent.signed[:customer_id] = customer.id
-      else
-        cookies.delete(:customer_id)
-        session[:customer_id] = customer.id
-      end
+      session[:customer_id] = customer.id
       flash.notice = 'ログインしました。'
-      redirect_to :customer_root
+      redirect_to :baukis_customer_root
     else
       flash.now.alert = 'メールアドレスまたはパスワードが正しくありません。'
       render action: 'new'
@@ -34,6 +29,15 @@ class Baukis::Customer::SessionsController < Baukis::Customer::Base
     cookies.delete(:customer_id)
     session.delete(:customer_id)
     flash.notice = 'ログアウトしました。'
-    redirect_to :customer_root
+    redirect_to :baukis_customer_root
+  end
+
+
+  private
+  def session_params
+    params.require(:baukis_customer_login_form).permit(
+        :email,
+        :password
+    )
   end
 end
