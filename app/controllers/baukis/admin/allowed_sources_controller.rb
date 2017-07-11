e20bfdbee4be336e5b65674faef7c2b35a769a23
k2works <kakimomokuri@gmail.com>
2017-07-11 17:31:24 +0900

    3.2 許可IPアドレスの管理 #57
    ・3.2.4 許可IPアドレスの新規登録フォーム
    
    EFROR-58

--- app/controllers/baukis/admin/allowed_sources_controller.rb
+++ app/controllers/baukis/admin/allowed_sources_controller.rb
@@ -1,5 +1,6 @@
 class Baukis::Admin::AllowedSourcesController < Baukis::Admin::Base
   def index
     @allowed_sources = Baukis::AllowedSource.order(:octet1, :octet2, :octet3, :octet4)
+    @new_allowed_source = Baukis::AllowedSource.new
   end
 end
--- app/models/baukis/allowed_source.rb
+++ app/models/baukis/allowed_source.rb
@@ -18,6 +18,8 @@
 #
 
 class Baukis::AllowedSource < ApplicationRecord
+  attr_accessor :last_octet
+
   validates :octet1, :octet2, :octet3, :octet4, presence: true, numericality: { only_integer: true, allow_blank: true }, inclusion: { in: 0..255, allow_blank: true }
   validates :octet4, uniqueness: { scope: [ :octet1, :octet2, :octet3 ], allow_blank: true }
 
