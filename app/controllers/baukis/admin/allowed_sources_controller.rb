37f90d0e69c3b7deee709144316af542041b5ee6
k2works <kakimomokuri@gmail.com>
2017-07-11 17:52:10 +0900

    3.2 許可IPアドレスの管理 #57
    ・3.2.5 許可IPアドレスの追加
    
    EFROR-58

--- app/controllers/baukis/admin/allowed_sources_controller.rb
+++ app/controllers/baukis/admin/allowed_sources_controller.rb
@@ -3,4 +3,22 @@ class Baukis::Admin::AllowedSourcesController < Baukis::Admin::Base
     @allowed_sources = Baukis::AllowedSource.order(:octet1, :octet2, :octet3, :octet4)
     @new_allowed_source = Baukis::AllowedSource.new
   end
+
+  def create
+    @new_allowed_source = Baukis::AllowedSource.new(allowed_source_params)
+    @new_allowed_source.namespace = 'staff'
+    if @new_allowed_source.save
+      flash.notice = '許可IPアドレスを追加しました。'
+      redirect_to action: 'index'
+    else
+      @allowed_sources = Baukis::AllowedSource.order(:octet1, :octet2, :octet3, :octet4)
+      flash.now.alert = '許可IPアドレスの値が正しくありません。'
+      render action: 'index'
+    end
+  end
+
+  private
+  def allowed_source_params
+    params.require(:baukis_allowed_source).permit(:octet1, :octet2, :octet3, :last_octet)
+  end
 end
