c1cbb967aae313a34bd5a8ef7696ddcc1d339e5f
k2works <kakimomokuri@gmail.com>
2017-07-27 16:29:45 +0900

    4.2 プログラム管理機能（前編） #55
    ・4.2.2 プログラムの詳細表示
    
    EFROR-26

--- app/controllers/baukis/staff/programs_controller.rb
+++ app/controllers/baukis/staff/programs_controller.rb
@@ -2,4 +2,8 @@ class Baukis::Staff::ProgramsController < Baukis::Staff::Base
   def index
     @programs = Baukis::Program.order(application_start_time: :desc).page(params[:page])
   end
+
+  def show
+    @program = Baukis::Program.find(params[:id])
+  end
 end
