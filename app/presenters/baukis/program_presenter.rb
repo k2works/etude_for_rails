1397456b23144bda1a4336635c48aef939f3625f
k2works <kakimomokuri@gmail.com>
2017-07-27 16:56:35 +0900

    4.3 パフォーマンスの改善 #55
    ・4.3.5 集計対象の変更による改善
    
    EFROR-26

--- app/presenters/baukis/program_presenter.rb
+++ app/presenters/baukis/program_presenter.rb
@@ -24,7 +24,7 @@ module Baukis
     end
 
     def number_of_applicants
-      number_with_delimiter(object.applicants.count)
+      number_with_delimiter(object.entries.count)
     end
 
     def registrant
