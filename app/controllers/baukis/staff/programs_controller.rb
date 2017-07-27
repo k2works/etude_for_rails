7159a505a0be1325a78e2271200c5c9dbc6cfdaf
k2works <kakimomokuri@gmail.com>
2017-07-27 16:49:37 +0900

    4.3 パフォーマンスの改善 #55
    ・4.3.3 includesメソッドによる改善
    
    EFROR-26

--- app/controllers/baukis/staff/programs_controller.rb
+++ app/controllers/baukis/staff/programs_controller.rb
@@ -1,6 +1,6 @@
 class Baukis::Staff::ProgramsController < Baukis::Staff::Base
   def index
-    @programs = Baukis::Program.order(application_start_time: :desc).page(params[:page])
+    @programs = Baukis::Program.order(application_start_time: :desc).includes(:registrant).page(params[:page])
   end
 
   def show
