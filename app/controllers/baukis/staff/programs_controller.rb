2df05c5e87d0285d481ce143e2d3929e0c7beeaa
k2works <kakimomokuri@gmail.com>
2017-07-28 13:01:44 +0900

    5.2 プログラム申込者管理機能 #55
    ・5.2.3 多数のオブジェクトの一括更新処理
    
    EFROR-26

--- app/controllers/baukis/staff/programs_controller.rb
+++ app/controllers/baukis/staff/programs_controller.rb
@@ -46,4 +46,13 @@ class Baukis::Staff::ProgramsController < Baukis::Staff::Base
     flash.notice = 'プログラムを削除しました。'
     redirect_to :baukis_staff_programs
   end
+
+  # PATCH
+  def entries
+    entries_form = Baukis::Staff::EntriesForm.new(Baukis::Program.find(params[:id]))
+    entries_form.assign_attributes(params[:form])
+    entries_form.save
+    flash.notice = 'プログラム申し込みフラグを更新しました。'
+    redirect_to :baukis_staff_programs
+  end
 end
