d1d3a50451f6e434368649ab66796e54dc125c63
k2works <kakimomokuri@gmail.com>
2017-07-28 10:40:39 +0900

    5.1 プログラム管理機能（後編） #55
    ・5.1.3 プログラムの新規登録と更新
    
    EFROR-26

--- app/models/baukis/program.rb
+++ app/models/baukis/program.rb
@@ -27,6 +27,9 @@ class Baukis::Program < ApplicationRecord
 
   attr_accessor :application_start_date, :application_start_hour, :application_start_minute, :application_end_date, :application_end_hour, :application_end_minute
 
+  before_validation :set_application_start_time
+  before_validation :set_application_end_time
+
   scope :listing, -> {
     joins('LEFT JOIN baukis_entries ON baukis_programs.id = baukis_entries.program_id')
         .select('baukis_programs.*, COUNT(baukis_entries.id) AS number_of_applicants')
@@ -34,4 +37,31 @@ class Baukis::Program < ApplicationRecord
         .order(application_start_time: :desc)
         .includes(:registrant)
   }
+
+  private
+  def set_application_start_time
+    return if application_start_date.blank?
+    Date.parse(application_start_date)
+    if t = Time.zone.parse(application_start_date)
+      self.application_start_time = t.advance(
+                                         hours: application_start_hour.to_i,
+                                         minutes: application_start_minute.to_i
+      )
+    end
+  rescue ArgumentError
+    self.application_start_time = nil
+  end
+
+  def set_application_end_time
+    return if application_end_date.blank?
+    Date.parse(application_end_date)
+    if t = Time.zone.parse(application_end_date)
+      self.application_end_time = t.advance(
+                                       hours: application_end_hour.to_i,
+                                       minutes: application_end_minute.to_i
+      )
+    end
+  rescue ArgumentError
+    self.application_end_time = nil
+  end
 end
