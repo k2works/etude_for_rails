537af3b0889458bbf183526d930cf99b3743a993
k2works <kakimomokuri@gmail.com>
2017-07-29 14:39:08 +0900

    8.1 顧客むけ問い合わせフォーム #56
    ・8.1.6 confirmアクション
    
    EFROR-29

--- app/controllers/baukis/customer/messages_controller.rb
+++ app/controllers/baukis/customer/messages_controller.rb
@@ -2,4 +2,21 @@ class Baukis::Customer::MessagesController < Baukis::Customer::Base
   def new
     @message = Baukis::CustomerMessage.new
   end
+
+  # POST
+  def confirm
+    @message = Baukis::CustomerMessage.new(customer_message_params)
+    @message.customer = current_customer
+    if @message.valid?
+      render action: 'confirm'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
+  private
+  def customer_message_params
+    params.require(:baukis_customer_message).permit(:subject, :body)
+  end
 end
