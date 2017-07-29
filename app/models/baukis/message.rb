10f7ff820fbd184cee3bbfd18c21f0964cf82ef0
k2works <kakimomokuri@gmail.com>
2017-07-29 13:56:28 +0900

    8.1 顧客むけ問い合わせフォーム #56
    ・8.1.3 バリデーション等
    
    EFROR-29

--- app/models/baukis/message.rb
+++ app/models/baukis/message.rb
@@ -36,4 +36,15 @@ class Baukis::Message < ApplicationRecord
   belongs_to :staff_member, :class_name => 'Baukis::StaffMember', optional: true
   belongs_to :root, :class_name => 'Baukis::Message', foreign_key: 'root_id', optional: true
   belongs_to :parent, :class_name => 'Baukis::Message', foreign_key: 'parent_id', optional: true
+
+  validates :subject, :body, presence: true
+  validates :subject, length: { maximum: 80, allow_blank: true }
+  validates :body, length: { maximum: 800, allow_blank: true }
+
+  before_create do
+    if parent
+      self.customer = parent.customer
+      self.root = parent.root || parent
+    end
+  end
 end
--- spec/factories/baukis_messages.rb
+++ spec/factories/baukis_messages.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :outbound_message, class: 'Baukis::CustomerMessage' do
     customer
     subject 'OutboundMessage'
+    body 'XXXXXXXXXXX'
   end
 
   factory :inbound_message, class: 'Baukis::StaffMessage' do
     customer
     subject 'InboundMessage'
+    body 'XXXXXXXXXXX'
   end
 end
