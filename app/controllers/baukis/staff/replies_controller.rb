4c3adaaa8353a9a005584686038d193c39d12bb9
k2works <kakimomokuri@gmail.com>
2017-07-31 10:25:26 +0900

    10.1 問い合わせへの返信機能 #56 #57
    ・10.1.4 確認画面
    
    EFROR-29
    EFROR-30

--- app/controllers/baukis/staff/replies_controller.rb
+++ app/controllers/baukis/staff/replies_controller.rb
@@ -5,8 +5,23 @@ class Baukis::Staff::RepliesController < Baukis::Staff::Base
     @reply = Baukis::StaffMessage.new
   end
 
+  # POST
+  def confirm
+    @reply = Baukis::StaffMessage.new(staff_message_params)
+    if @reply.valid?
+      render action: 'confirm'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
   private
   def prepare_message
     @message = Baukis::CustomerMessage.find(params[:message_id])
+  end
+
+  def staff_message_params
+    params.require(:baukis_staff_message).permit(:subject, :body)
   end
 end
