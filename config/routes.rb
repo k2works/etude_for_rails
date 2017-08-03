c801aa530a8277475e6f2b07d7205fd59c8acfe7
k2works <kakimomokuri@gmail.com>
2017-08-03 15:59:39 +0900

    レイアウトを作成する #63
    ・5.3.3 名前付きルート
    
    EFROR-45

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,6 @@
 #                     rails_tutorial_sample_help GET    /rails_tutorial/sample/help(.:format)                               rails_tutorial/sample/static_pages#help
 #                    rails_tutorial_sample_about GET    /rails_tutorial/sample/about(.:format)                              rails_tutorial/sample/static_pages#about
 #                  rails_tutorial_sample_contact GET    /rails_tutorial/sample/contact(.:format)                            rails_tutorial/sample/static_pages#contact
-#                   rails_tutorial_sample_signup GET    /rails_tutorial/sample/signup(.:format)                             rails_tutorial/sample/#signup
-#                    rails_tutorial_sample_login GET    /rails_tutorial/sample/login(.:format)                              rails_tutorial/sample/#login
 #                              baukis_staff_root GET    /baukis(.:format)                                                   baukis/staff/top#index
 #                             baukis_staff_login GET    /baukis/login(.:format)                                             baukis/staff/sessions#new
 #                           baukis_staff_session DELETE /baukis/session(.:format)                                           baukis/staff/sessions#destroy
@@ -211,8 +209,6 @@ Rails.application.routes.draw do
       get '/help', to:'static_pages#help'
       get '/about', to:'static_pages#about'
       get '/contact', to:'static_pages#contact'
-      get 'signup' => '#signup', as: :signup
-      get 'login' => '#login', as: :login
     end
   end
 
