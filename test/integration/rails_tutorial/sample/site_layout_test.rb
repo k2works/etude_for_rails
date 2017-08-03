2810e76a5c72d8dbead4773fe667f0d3524b1935
k2works <kakimomokuri@gmail.com>
2017-08-03 16:06:08 +0900

    レイアウトを作成する #63
    ・5.3.4 リンクのテスト
    
    EFROR-45

---
+++ test/integration/rails_tutorial/sample/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get rails_tutorial_sample_root_path
+    assert_template 'rails_tutorial/sample/static_pages/home'
+    assert_select "a[href=?]", rails_tutorial_sample_root_path, count: 2
+    assert_select "a[href=?]", rails_tutorial_sample_help_path
+    assert_select "a[href=?]", rails_tutorial_sample_about_path
+    assert_select "a[href=?]", rails_tutorial_sample_contact_path
+  end
+end
