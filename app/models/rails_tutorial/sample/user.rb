aa9f18dc16ac82b362e7f060204c2cc93b775082
k2works <kakimomokuri@gmail.com>
2017-08-04 13:27:07 +0900

    ユーザーのモデルを作成する #64
    ・6.2.4 フォーマットを検証する
    
    EFROR-46

--- app/models/rails_tutorial/sample/user.rb
+++ app/models/rails_tutorial/sample/user.rb
@@ -11,5 +11,6 @@
 
 class RailsTutorial::Sample::User < ApplicationRecord
   validates :name, presence:true,length:{ maximum:50 }
-  validates :email, presence:true, length:{ maximum:225 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:true, length:{ maximum:225 },format: { with:VALID_EMAIL_REGEX }
 end
