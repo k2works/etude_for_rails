42fdb879123dd1e49843966a0d92d67ca2192794
k2works <kakimomokuri@gmail.com>
2017-08-04 14:04:47 +0900

    ユーザーのモデルを作成する #64
    ・演習
    
    EFROR-46

--- app/models/rails_tutorial/sample/user.rb
+++ app/models/rails_tutorial/sample/user.rb
@@ -14,7 +14,7 @@
 #
 
 class RailsTutorial::Sample::User < ApplicationRecord
-  before_save { self.email=email.downcase }
+  before_save { email.downcase! }
   validates :name, presence:true,length:{ maximum:50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true, length:{ maximum:225 },
