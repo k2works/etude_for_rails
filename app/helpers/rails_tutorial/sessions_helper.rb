5b293382a0b0f90e2ae037a3076e160d7fba872d
k2works <kakimomokuri@gmail.com>
2017-08-05 10:20:14 +0900

    基本的なログイン機構 #66
    ・8.2.2 現在のユーザー
    
    EFROR-48

--- app/helpers/rails_tutorial/sessions_helper.rb
+++ app/helpers/rails_tutorial/sessions_helper.rb
@@ -2,7 +2,11 @@ module RailsTutorial
   module SessionsHelper
     # 渡されたユーザーでログインする
     def log_in(user)
-      session[:user_id] = user.id
+      session[:rails_tutorial_sample_user_id] = user.id
+    end
+    # 現在ログイン中のユーザーを返す (いる場合)
+    def current_user
+      @current_user ||= RailsTutorial::Sample::User.find_by(id: session[:rails_tutorial_sample_user_id])
     end
   end
 end
