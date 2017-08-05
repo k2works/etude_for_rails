afa53c3bb7b0ca96eded4c46042b4331711817ab
k2works <kakimomokuri@gmail.com>
2017-08-05 09:54:51 +0900

    基本的なログイン機構 #66
    ・8.1.5 フラッシュのテスト
    
    EFROR-48

--- app/controllers/rails_tutorial/sample/sessions_controller.rb
+++ app/controllers/rails_tutorial/sample/sessions_controller.rb
@@ -7,7 +7,7 @@ class RailsTutorial::Sample::SessionsController < RailsTutorial::ApplicationCont
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
       render 'new'
     end
   end
