a530875718b56359efc94c02c13257812fd0ec7e
k2works <kakimomokuri@gmail.com>
2017-08-07 15:36:21 +0900

    パスワードの再設定 #70
    ・12.2.1 パスワード再設定のメールとテンプレート
    
    EFROR-52

--- spec/mailers/previews/rails_tutorial/sample/user_mailer_preview.rb
+++ spec/mailers/previews/rails_tutorial/sample/user_mailer_preview.rb
@@ -10,7 +10,9 @@ class RailsTutorial::Sample::UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/rails_tutorial/sample/user_mailer/password_reset
   def password_reset
-    RailsTutorial::Sample::UserMailer.password_reset
+    user = RailsTutorial::Sample::User.first
+    user.reset_token = RailsTutorial::Sample::User.new_token
+    RailsTutorial::Sample::UserMailer.password_reset(user)
   end
 
 end
