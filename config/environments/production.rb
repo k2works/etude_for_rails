f55b73576a36bfda3e6d9a0c4a8349495fb57ea5
k2works <kakimomokuri@gmail.com>
2017-08-07 14:47:28 +0900

    アカウントの有効化 #69
    ・11.4 本番環境でのメール送信
    
    EFROR-51

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,19 @@ Rails.application.configure do
   config.baukis = {
       restrict_ip_addresses: false
   }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'etude-for-rails.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+
 end
