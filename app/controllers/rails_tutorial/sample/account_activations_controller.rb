46690a6f7b1f1a4622c83a4863b0ded74dc96c0b
k2works <kakimomokuri@gmail.com>
2017-08-07 13:55:36 +0900

    アカウントの有効化 #69
    ・11.3.2 editアクションで有効化
    
    EFROR-51

--- app/controllers/rails_tutorial/sample/account_activations_controller.rb
+++ app/controllers/rails_tutorial/sample/account_activations_controller.rb
@@ -1,2 +1,15 @@
-class RailsTutorial::Sample::AccountActivationsController < ApplicationController
+class RailsTutorial::Sample::AccountActivationsController < RailsTutorial::ApplicationController
+  def edit
+    user = RailsTutorial::Sample::User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to rails_tutorial_sample_root_url
+    end
+  end
 end
