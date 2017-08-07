e22e1ed19a5503be1a72f2402f2614d1084f040d
k2works <kakimomokuri@gmail.com>
2017-08-07 12:48:26 +0900

    アカウントの有効化 #69
    ・11.2.3 送信メールのテスト
    
    EFROR-51

---
+++ test/mailers/rails_tutorial/sample/user_mailer_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RailsTutorial::Sample::UserMailerTest < ActionMailer::TestCase
+  test "account_activation" do
+    user = rails_tutorial_sample_users(:michael)
+    user.activation_token = RailsTutorial::Sample::User.new_token
+    mail = RailsTutorial::Sample::UserMailer.account_activation(user)
+    assert_equal "Account activation", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name,               mail.body.encoded
+    assert_match user.activation_token,   mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+  end
+end
