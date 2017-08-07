da49306797ac8d56943c56446624e3a6946bd46c
k2works <kakimomokuri@gmail.com>
2017-08-07 18:56:19 +0900

    ユーザーのマイクロポスト #71
    ・13.3.4 マイクロポストを削除する
    
    EFROR-53

--- app/controllers/rails_tutorial/sample/microposts_controller.rb
+++ app/controllers/rails_tutorial/sample/microposts_controller.rb
@@ -1,5 +1,6 @@
 class RailsTutorial::Sample::MicropostsController < RailsTutorial::ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -12,11 +13,19 @@ class RailsTutorial::Sample::MicropostsController < RailsTutorial::ApplicationCo
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || rails_tutorial_sample_root_url
   end
 
   private
 
   def micropost_params
     params.require(:rails_tutorial_sample_micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
