35ea406447a4777855b4102c0b8d310e2e2d3c73
k2works <kakimomokuri@gmail.com>
2017-08-07 12:33:32 +0900

    アカウントの有効化 #69
    ・11.2.2 送信メールのプレビュー
    
    EFROR-51

--- spec/mailers/previews/rails_tutorial/sample/user_mailer_preview.rb
+++ spec/mailers/previews/rails_tutorial/sample/user_mailer_preview.rb
@@ -3,7 +3,9 @@ class RailsTutorial::Sample::UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/rails_tutorial/sample/user_mailer/account_activation
   def account_activation
-    RailsTutorial::Sample::UserMailer.account_activation
+    user = RailsTutorial::Sample::User.first
+    user.activation_token = RailsTutorial::Sample::User.new_token
+    RailsTutorial::Sample::UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/rails_tutorial/sample/user_mailer/password_reset
