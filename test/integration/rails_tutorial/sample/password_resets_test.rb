74292e7fb2f664f519ccbfe404047d8e75a1ce4c
k2works <kakimomokuri@gmail.com>
2017-08-07 16:18:04 +0900

    パスワードの再設定 #70
    ・12.3.3 パスワードの再設定をテストする
    
    EFROR-52

---
+++ test/integration/rails_tutorial/sample/password_resets_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class RailsTutorial::Sample::PasswordResetsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+    @user = rails_tutorial_sample_users(:michael)
+  end
+
+  test "password resets" do
+    get new_rails_tutorial_sample_password_reset_path
+    assert_template 'password_resets/new'
+    # メールアドレスが無効
+    post rails_tutorial_sample_password_resets_path, params: { password_reset: { email: "" } }
+    assert_not flash.empty?
+    assert_template 'rails_tutorial/sample/password_resets/new'
+    # メールアドレスが有効
+    post rails_tutorial_sample_password_resets_path,
+         params: { password_reset: { email: @user.email } }
+    assert_not_equal @user.reset_digest, @user.reload.reset_digest
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_not flash.empty?
+    assert_redirected_to rails_tutorial_sample_root_url
+    # パスワード再設定フォームのテスト
+    user = assigns(:user)
+    # メールアドレスが無効
+    get edit_rails_tutorial_sample_password_reset_path(user.reset_token, email: "")
+    assert_redirected_to rails_tutorial_sample_root_url
+    # 無効なユーザー
+    user.toggle!(:activated)
+    get edit_rails_tutorial_sample_password_reset_path(user.reset_token, email: user.email)
+    assert_redirected_to rails_tutorial_sample_root_url
+    user.toggle!(:activated)
+    # メールアドレスが有効で、トークンが無効
+    get edit_rails_tutorial_sample_password_reset_path('wrong token', email: user.email)
+    assert_redirected_to rails_tutorial_sample_root_url
+    # メールアドレスもトークンも有効
+    get edit_rails_tutorial_sample_password_reset_path(user.reset_token, email: user.email)
+    assert_template 'password_resets/edit'
+    assert_select "input[name=email][type=hidden][value=?]", user.email
+    # 無効なパスワードとパスワード確認
+    patch rails_tutorial_sample_password_reset_path(user.reset_token),
+          params: { email: user.email,
+                    rails_tutorial_sample_user: { password:              "foobaz",
+                            password_confirmation: "barquux" } }
+    assert_select 'div#error_explanation'
+    # パスワードが空
+    patch rails_tutorial_sample_password_reset_path(user.reset_token),
+          params: { email: user.email,
+                    rails_tutorial_sample_user: { password:              "",
+                            password_confirmation: "" } }
+    assert_select 'div#error_explanation'
+    # 有効なパスワードとパスワード確認
+    patch rails_tutorial_sample_password_reset_path(user.reset_token),
+          params: { email: user.email,
+                    rails_tutorial_sample_user: { password:              "foobaz",
+                            password_confirmation: "foobaz" } }
+    assert is_logged_in?
+    assert_not flash.empty?
+    assert_redirected_to user
+  end
+end
