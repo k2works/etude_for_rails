6b49f2b8bc12ce7944eef9f5e41ee517fc93b041
k2works <kakimomokuri@gmail.com>
2017-08-10 13:22:52 +0900

    ユーザーをフォローする #72
    ・14.2.1 フォローのサンプルデータ
    
    EFROR-54

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,7 @@ table_names.each do |table_name|
   end
 end
 
+# ユーザー
 RailsTutorial::Sample::User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -36,8 +37,17 @@ RailsTutorial::Sample::User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = RailsTutorial::Sample::User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = RailsTutorial::Sample::User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
