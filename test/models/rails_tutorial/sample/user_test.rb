ecd7f9f2fbd9bca861bf26988fbad93ede8ec7e1
k2works <kakimomokuri@gmail.com>
2017-08-10 13:13:04 +0900

    ユーザーをフォローする #72
    ・14.1.4 フォローしているユーザー
    
    EFROR-54

--- test/models/rails_tutorial/sample/user_test.rb
+++ test/models/rails_tutorial/sample/user_test.rb
@@ -94,4 +94,14 @@ class RailsTutorial::Sample::UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    michael = rails_tutorial_sample_users(:michael)
+    archer  = rails_tutorial_sample_users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
