d67e931c2e003e7e471d16fcabc65c966f473552
k2works <kakimomokuri@gmail.com>
2017-08-17 17:47:42 +0900

    アルゴリズムを変更する：Template Method #73
    ・3.1 運命に負けないために
    
    EFROR-60

--- app/domains/design_pattern/template_method/report.rb
+++ app/domains/design_pattern/template_method/report.rb
@@ -6,17 +6,32 @@ module DesignPattern
         @text = ['順調', '最高の調子']
       end
 
-      def output_report
-        puts('<html>')
-        puts(' <head>')
-        puts(" <title>#{@title}</title>")
-        puts(' </head>')
-        puts(' <body>')
-        @text.each do |line|
-          puts(" <p>#{line}</p>")
+      def output_report(format)
+        if format == :plain
+          puts("*** #{@title} ***")
+        elsif format == :html
+          puts('<html>')
+          puts(' <head>')
+          puts(" <title>#{@title}</title>")
+          puts(' </head>')
+          puts(' <body>')
+        else
+          raise "Unknown format: #{format}"
         end
-        puts(' </body>')
-        puts('</html>')
+
+
+        @text.each do |line|
+          if format == :plain
+            puts(line)
+          else
+            puts(" <p>#{line}</p>")
+          end
+        end
+
+        if format == :html
+          puts(' </body>')
+          puts('</html>')
+        end
       end
     end
   end
--- spec/domains/design_pattern/template_method/report_spec.rb
+++ spec/domains/design_pattern/template_method/report_spec.rb
@@ -3,9 +3,10 @@ include DesignPattern
 
 describe DesignPattern::TemplateMethod::Report do
   describe '#output_report' do
-    it 'output html' do
-      report = DesignPattern::TemplateMethod::Report.new
-      expected = <<-EOS
+    context 'format is HTML' do
+      it 'output' do
+        report = DesignPattern::TemplateMethod::Report.new
+        expected = <<-EOS
 <html>
  <head>
  <title>月次報告</title>
@@ -15,9 +16,23 @@ describe DesignPattern::TemplateMethod::Report do
  <p>最高の調子</p>
  </body>
 </html>
-EOS
+        EOS
 
-      expect { report.output_report }.to output(expected).to_stdout
+        expect { report.output_report(:html) }.to output(expected).to_stdout
+      end
+    end
+
+    context 'format is Plain text' do
+      it 'output' do
+        report = DesignPattern::TemplateMethod::Report.new
+        expected = <<-EOS
+*** 月次報告 ***
+順調
+最高の調子
+        EOS
+
+        expect { report.output_report(:plain) }.to output(expected).to_stdout
+      end
     end
   end
 end
