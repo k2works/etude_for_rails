c803e2d5f00d9aa57c1b3dd9cc3d9e58149c554b
k2works <kakimomokuri@gmail.com>
2017-08-17 18:09:23 +0900

    アルゴリズムを変更する：Template Method #73
    ・3.2 変わるものと変わらないものを分離する
    
    EFROR-60

---
+++ app/domains/design_pattern/template_method/plain_text_report.rb
@@ -0,0 +1,26 @@
+module DesignPattern
+  module TemplateMethod
+    class PlainTextReport < Report
+      def output_start
+      end
+
+      def output_head
+        puts("**** #{@title} ****")
+        puts
+      end
+
+      def output_body_start
+      end
+
+      def output_line(line)
+        puts(line)
+      end
+
+      def output_body_end
+      end
+
+      def output_end
+      end
+    end
+  end
+end
--- spec/domains/design_pattern/template_method/report_spec.rb
+++ spec/domains/design_pattern/template_method/report_spec.rb
@@ -5,33 +5,34 @@ describe DesignPattern::TemplateMethod::Report do
   describe '#output_report' do
     context 'format is HTML' do
       it 'output' do
-        report = DesignPattern::TemplateMethod::Report.new
+        report = DesignPattern::TemplateMethod::HTMLReport.new
         expected = <<-EOS
 <html>
  <head>
  <title>月次報告</title>
  </head>
- <body>
+<body>
  <p>順調</p>
  <p>最高の調子</p>
- </body>
+</body>
 </html>
         EOS
 
-        expect { report.output_report(:html) }.to output(expected).to_stdout
+        expect { report.output_report }.to output(expected).to_stdout
       end
     end
 
     context 'format is Plain text' do
       it 'output' do
-        report = DesignPattern::TemplateMethod::Report.new
+        report = DesignPattern::TemplateMethod::PlainTextReport.new
         expected = <<-EOS
-*** 月次報告 ***
+**** 月次報告 ****
+
 順調
 最高の調子
         EOS
 
-        expect { report.output_report(:plain) }.to output(expected).to_stdout
+        expect { report.output_report }.to output(expected).to_stdout
       end
     end
   end
