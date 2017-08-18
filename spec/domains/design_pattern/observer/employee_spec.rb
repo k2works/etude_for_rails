4ddcf46f2852d08e7d5469f0587d950239599c8b
k2works <kakimomokuri@gmail.com>
2017-08-18 14:12:43 +0900

    変更に追従する：Observer #75
    ・5.4 オブザーバとしてのコードブロック
    
    EFROR-62

--- spec/domains/design_pattern/observer/employee_spec.rb
+++ spec/domains/design_pattern/observer/employee_spec.rb
@@ -65,3 +65,22 @@ Fredに新しい税金の請求書を送ります！
     end
   end
 end
+
+describe DesignPattern::Observer::Employee3 do
+  describe '#salary' do
+    it 'observe payroll' do
+      fred = DesignPattern::Observer::Employee3.new('Fred', 'Crane Operator', 30000)
+      fred.add_observer do |change_employee|
+        puts("Cut a new check for #{change_employee.name}!")
+        puts("His salary is now #{change_employee.salary}!")
+      end
+
+      expected = <<-EOS
+Cut a new check for Fred!
+His salary is now 35000!
+      EOS
+
+      expect { fred.salary = 35000 }.to output(expected).to_stdout
+    end
+  end
+end
