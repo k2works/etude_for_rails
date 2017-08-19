0430054170c8db75541664c997a37b82be717959
k2works <kakimomokuri@gmail.com>
2017-08-19 16:52:29 +0900

    ギャップを埋める：Adapter #79
    ・9.3 アダプタの代替手段
    
    EFROR-66

--- app/domains/design_pattern/adapter/british_text_object.rb
+++ app/domains/design_pattern/adapter/british_text_object.rb
@@ -9,5 +9,19 @@ module DesignPattern
         @colour = colour
       end
     end
+
+    class BritishTextObject
+      def color
+        return colour
+      end
+
+      def text
+        return string
+      end
+
+      def size_inches
+        return size_mm / 25.4
+      end
+    end
   end
 end
--- spec/domains/design_pattern/adapter/renderer_spec.rb
+++ spec/domains/design_pattern/adapter/renderer_spec.rb
@@ -28,5 +28,18 @@ Color is Red
 
       expect { renderer.render(adapter) }.to output(expected).to_stdout
     end
+
+    it 'render british text object using singleton class' do
+      bto = DesignPattern::Adapter::BritishTextObject.new('String',50.8,'Red')
+      renderer = DesignPattern::Adapter::Renderer.new
+
+      expected = <<-EOF
+Text is String
+2.0 inch
+Color is Red
+      EOF
+
+      expect { renderer.render(bto) }.to output(expected).to_stdout
+    end
   end
 end
