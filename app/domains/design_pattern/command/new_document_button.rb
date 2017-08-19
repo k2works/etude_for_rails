7a3356ab2a08b8d263ce38153aaa08b7806910c8
k2works <kakimomokuri@gmail.com>
2017-08-19 11:03:34 +0900

    命令を実行する：Command #78
    ・8.1 サブクラスの爆発
    
    EFROR-65

---
+++ app/domains/design_pattern/command/new_document_button.rb
@@ -0,0 +1,9 @@
+module DesignPattern
+  module Command
+    class NewDocumentButton < SlickButton
+      #
+      # 新しい文書を作成
+      #
+    end
+  end
+end
---
+++ app/domains/design_pattern/command/slick_button.rb
@@ -0,0 +1,15 @@
+module DesignPattern
+  module Command
+    class SlickButton
+      #
+      # ボタンの描画と管理のためのコード
+      # コードを省略    
+
+      def on_button_push
+        #
+        # ボタンが押された時に行うこと
+        # 
+      end
+    end  
+  end
+end
