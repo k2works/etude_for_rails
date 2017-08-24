fae941e53ebd917bfc3a17080fc33aefdf7f28f8
k2works <kakimomokuri@gmail.com>
2017-08-24 19:09:18 +0900

    オブジェクトを改良する：Decorator #81
    ・11.4 動的なDecoratorパターン
    
    EFROR-68

--- spec/domains/design_pattern/decorator/write_decorator_spec.rb
+++ spec/domains/design_pattern/decorator/write_decorator_spec.rb
@@ -48,5 +48,69 @@ describe DesignPattern::Decorator::WriterDecorator do
 
       File.delete(file_out)
     end
+
+    it 'write line using alias' do
+      file_out = Pathname.new('tmp/out.txt')
+      w = DesignPattern::Decorator::SimpleWriter.new(file_out)
+
+      class << w
+         alias old_write_line write_line
+
+         def write_line(line)
+           old_write_line("#{Time.new}: #{line}")
+         end
+      end
+
+      w.write_line('Hello out there')
+      w.close
+
+      expect(file_out).to exist
+      expect(File.open(file_out).read).to include('Hello out there¥n')
+
+      File.delete(file_out)
+    end
+
+    it 'write line using module' do
+      module TimeStampingWriter
+        def write_line(line)
+          super("#{Time.new}: #{line}")
+        end
+      end
+
+      module CheckSummingWriter
+        attr_reader :check_sum
+
+        def write_line(line)
+          @check_sum = 0 unless @check_sum
+          line.each_byte { |byte| @check_sum = (@check_sum + byte) % 256 }
+          super(line)
+          @check_sum += "\n".ord % 256
+        end
+      end
+
+      module NumberingWriter
+        attr_reader :line_number
+
+        def write_line(line)
+          @line_number = 1 unless @line_number
+          super("#{@line_number}: #{line}")
+          @line_number += 1
+        end
+      end
+
+      file_out = Pathname.new('tmp/out.txt')
+      w = DesignPattern::Decorator::SimpleWriter.new(file_out)
+      w.extend(CheckSummingWriter)
+      w.extend(NumberingWriter)
+
+      w.write_line('Hello out there')
+      w.close
+
+      expect(file_out).to exist
+      expect(File.open(file_out).read).to eq('1: Hello out there¥n')
+      expect(w.check_sum).to be > 0
+
+      File.delete(file_out)
+    end
   end
 end
