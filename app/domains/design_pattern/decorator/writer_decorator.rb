4775e2acef31f26dc409bbfa4bf2ad7513948eac
k2works <kakimomokuri@gmail.com>
2017-08-24 18:50:29 +0900

    オブジェクトを改良する：Decorator #81
    ・11.3 憂鬱な委譲を簡単に
    
    EFROR-68

--- app/domains/design_pattern/decorator/writer_decorator.rb
+++ app/domains/design_pattern/decorator/writer_decorator.rb
@@ -1,24 +1,14 @@
+require 'forwardable'
+
 module DesignPattern
   module Decorator
     class WriterDecorator
+      extend Forwardable
+
+      def_delegators :@real_writer, :write_line, :rewind, :pos, :close
+
       def initialize(real_writer)
         @real_writer = real_writer
-      end
-
-      def write_line(line)
-        @real_writer.write_line(line)
-      end
-
-      def pos
-        @real_writer.pos
-      end
-
-      def rewind
-        @real_writer.rewind
-      end
-
-      def close
-        @real_writer.close
       end
     end
   end
