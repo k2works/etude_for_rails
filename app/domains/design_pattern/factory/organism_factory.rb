bff355390caa6e395fe5bf6b4da3ff13c1c31431
k2works <kakimomokuri@gmail.com>
2017-08-28 15:06:14 +0900

    正しいクラスを選び出す：Factory #83
    ・13.7 クラスもただのオブジェクト（再び）
    
    EFROR-70

---
+++ app/domains/design_pattern/factory/organism_factory.rb
@@ -0,0 +1,18 @@
+module DesignPattern
+  module Factory
+    class OrganismFactory
+      def initialize(plant_class, animal_class)
+        @plant_class = plant_class
+        @animal_class = animal_class
+      end
+
+      def new_animal(name)
+        @animal_class.new(name)
+      end
+
+      def new_plant(name)
+        @plant_class.new(name)
+      end
+    end
+  end
+end
--- spec/domains/design_pattern/factory/habitat_spec.rb
+++ spec/domains/design_pattern/factory/habitat_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe DesignPattern::Factory::Habitat do
   describe '#simulate_ond_day' do
     it 'simulate one day duck pond' do
-      pond = DesignPattern::Factory::Habitat.new(3, 0, DesignPattern::Factory::PondOrganismFactory1.new)
+      habitat = DesignPattern::Factory::Habitat.new(3, 0, DesignPattern::Factory::PondOrganismFactory1.new)
       expected = <<-EOS
 アヒル 動物0がガーガー鳴いています。
 アヒル 動物1がガーガー鳴いています。
@@ -16,11 +16,11 @@ describe DesignPattern::Factory::Habitat do
 アヒル 動物2は静かに眠っています。
       EOS
 
-      expect {pond.simulate_one_day}.to output(expected).to_stdout
+      expect {habitat.simulate_one_day}.to output(expected).to_stdout
     end
 
     it 'simulate one day frog habitat' do
-      pond = DesignPattern::Factory::Habitat.new(3, 0, DesignPattern::Factory::PondOrganismFactory2.new)
+      habitat = DesignPattern::Factory::Habitat.new(3, 0, DesignPattern::Factory::PondOrganismFactory2.new)
       expected = <<-EOS
 カエル 動物0はゲロゲロと鳴いています。
 カエル 動物1はゲロゲロと鳴いています。
@@ -33,11 +33,11 @@ describe DesignPattern::Factory::Habitat do
 カエル 動物2は眠りません。一晩中ゲロゲロ鳴いています。
       EOS
 
-      expect {pond.simulate_one_day}.to output(expected).to_stdout
+      expect {habitat.simulate_one_day}.to output(expected).to_stdout
     end
 
     it 'simulate one day duck waterlily habitat' do
-      pond = DesignPattern::Factory::Habitat.new(2, 4, DesignPattern::Factory::PondOrganismFactory1.new)
+      habitat = DesignPattern::Factory::Habitat.new(2, 4, DesignPattern::Factory::PondOrganismFactory1.new)
       expected = <<-EOS
 スイレン 植物0は浮きながら日光を浴びて育ちます。
 スイレン 植物1は浮きながら日光を浴びて育ちます。
@@ -51,11 +51,11 @@ describe DesignPattern::Factory::Habitat do
 アヒル 動物1は静かに眠っています。
       EOS
 
-      expect {pond.simulate_one_day}.to output(expected).to_stdout
+      expect {habitat.simulate_one_day}.to output(expected).to_stdout
     end
 
     it 'simulate one day tiger tree habitat' do
-      pond = DesignPattern::Factory::Habitat.new(1, 4, DesignPattern::Factory::JungleOrganismFactory.new)
+      habitat = DesignPattern::Factory::Habitat.new(1, 4, DesignPattern::Factory::JungleOrganismFactory.new)
       expected = <<-EOS
 樹木 植物0が高く育っています。
 樹木 植物1が高く育っています。
@@ -66,7 +66,44 @@ describe DesignPattern::Factory::Habitat do
 トラ 動物0は眠たくなったら眠ります。
       EOS
 
-      expect {pond.simulate_one_day}.to output(expected).to_stdout
+      expect {habitat.simulate_one_day}.to output(expected).to_stdout
+    end
+
+    context 'Using OrganismFactory' do
+      it 'simulate jungle organism' do
+        jungle_organism_factory = DesignPattern::Factory::OrganismFactory.new(DesignPattern::Factory::Tree, DesignPattern::Factory::Tiger)
+        habitat = DesignPattern::Factory::Habitat.new(1, 4, jungle_organism_factory)
+        expected = <<-EOS
+樹木 植物0が高く育っています。
+樹木 植物1が高く育っています。
+樹木 植物2が高く育っています。
+樹木 植物3が高く育っています。
+トラ 動物0はガオーとほえています。
+トラ 動物0は食べたいものを何でも食べます。
+トラ 動物0は眠たくなったら眠ります。
+        EOS
+
+        expect {habitat.simulate_one_day}.to output(expected).to_stdout
+      end
+
+      it 'simulate pond organism' do
+        pond_organism_factory = DesignPattern::Factory::OrganismFactory.new(DesignPattern::Factory::WaterLily, DesignPattern::Factory::Frog)
+        habitat = DesignPattern::Factory::Habitat.new(2, 4, pond_organism_factory)
+        expected = <<-EOS
+スイレン 植物0は浮きながら日光を浴びて育ちます。
+スイレン 植物1は浮きながら日光を浴びて育ちます。
+スイレン 植物2は浮きながら日光を浴びて育ちます。
+スイレン 植物3は浮きながら日光を浴びて育ちます。
+カエル 動物0はゲロゲロと鳴いています。
+カエル 動物1はゲロゲロと鳴いています。
+カエル 動物0は食事中です。
+カエル 動物1は食事中です。
+カエル 動物0は眠りません。一晩中ゲロゲロ鳴いています。
+カエル 動物1は眠りません。一晩中ゲロゲロ鳴いています。
+        EOS
+
+        expect {habitat.simulate_one_day}.to output(expected).to_stdout
+      end
     end
   end
 end
