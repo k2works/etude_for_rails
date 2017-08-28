06482eddc1590d390d6829052e94fbf44906e935
k2works <kakimomokuri@gmail.com>
2017-08-28 18:02:58 +0900

    オブジェクトを組み立てやすくする：Builder #84
    ・14.5 マジックメソッドを使ったビルダ
    
    EFROR-71

--- app/domains/design_pattern/builder/computer_builder.rb
+++ app/domains/design_pattern/builder/computer_builder.rb
@@ -14,6 +14,18 @@ module DesignPattern
       def memory_size(size_in_mb)
         @computer.motherboard.memory_size = size_in_mb
       end
+
+      def method_missing(name, *args)
+        words = name.to_s.split("_")
+        return super(name, *args) unless words.shift == 'add'
+        words.each do |word|
+          next if word == 'and'
+          add_cd if word == 'cd'
+          add_dvd if word == 'dvd'
+          add_hard_disk(100000) if word == 'harddisk'
+          turbo if word == 'turbo'
+        end
+      end
     end
   end
 end
--- spec/domains/design_pattern/builder/computer_builder_spec.rb
+++ spec/domains/design_pattern/builder/computer_builder_spec.rb
@@ -46,6 +46,20 @@ describe DesignPattern::Builder::ComputerBuilder do
 
         expect { builder.computer }.to raise_error(RuntimeError, "No hard disk.")
       end
+
+      it 'can build using magic method' do
+        builder = DesignPattern::Builder::DeskTopBuilder.new
+        builder.add_dvd_and_harddisk
+        computer = builder.computer
+
+        expect(computer.motherboard.cpu.class).to eq(DesignPattern::Builder::BasicCPU)
+        expect(computer.drives.first.type).to eq(:dvd)
+        expect(computer.drives.first.size).to eq(4000)
+        expect(computer.drives.first.writable).to be false
+        expect(computer.drives.second.type).to eq(:hard_disk)
+        expect(computer.drives.second.size).to eq(100000)
+        expect(computer.drives.second.writable).to be true
+      end
     end
 
     context 'Using LaptopBuilder' do
@@ -109,6 +123,23 @@ describe DesignPattern::Builder::ComputerBuilder do
         computer2 = builder.computer
 
         expect(computer1).not_to eq(computer2)
+      end
+
+      it 'can build using magic method' do
+        builder = DesignPattern::Builder::LaptopBuilder.new
+        builder.add_turbo_and_cd_and_dvd_and_harddisk
+        computer = builder.computer
+
+        expect(computer.motherboard.cpu.class).to eq(DesignPattern::Builder::TurboCPU)
+        expect(computer.drives.first.type).to eq(:cd)
+        expect(computer.drives.first.size).to eq(760)
+        expect(computer.drives.first.writable).to be false
+        expect(computer.drives.second.type).to eq(:dvd)
+        expect(computer.drives.second.size).to eq(4000)
+        expect(computer.drives.second.writable).to be false
+        expect(computer.drives.third.type).to eq(:hard_disk)
+        expect(computer.drives.third.size).to eq(100000)
+        expect(computer.drives.third.writable).to be true
       end
     end
   end
