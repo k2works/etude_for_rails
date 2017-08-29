ed316c2e61178cac9e0beab554397150c31053d2
k2works <kakimomokuri@gmail.com>
2017-08-29 14:56:39 +0900

    オリジナル言語を作る : Domain-Specific Languages #86
    ・16.5 DSLを読み込む
    
    EFROR-73

--- app/domains/design_pattern/dsl/packrat.rb
+++ app/domains/design_pattern/dsl/packrat.rb
@@ -6,17 +6,21 @@ require_relative '../../../../app/domains/design_pattern/interpreter/file_name.r
 require_relative '../../../../app/domains/design_pattern/interpreter/not.rb'
 require_relative '../../../../app/domains/design_pattern/interpreter/or.rb'
 require_relative '../../../../app/domains/design_pattern/interpreter/writable.rb'
+require_relative '../../../../app/domains/design_pattern/dsl/backup.rb'
+require_relative '../../../../app/domains/design_pattern/dsl/data_source.rb'
+include DesignPattern::DSL
 
 def backup(dir, find_expression= DesignPattern::Interpreter::All.new)
-  puts "Backup called, source dir=#{dir} find expr=#{find_expression}"
+  Backup.instance.data_sources << DataSource.new(dir, find_expression)
 end
 
 def to(backup_directory)
-  puts "To called, backup dir=#{backup_directory}"
+  Backup.instance.backup_directory = backup_directory
 end
 
 def interval(minutes)
-  puts "Interval called, interval = #{minutes} minutes"
+  Backup.instance.interval = minutes
 end
 
 eval(File.read('backup.pr'))
+Backup.instance.run
