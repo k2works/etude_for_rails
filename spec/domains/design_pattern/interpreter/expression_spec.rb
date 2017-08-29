bdd7b650aa28301b00593d9945230967f4fd1ba8
k2works <kakimomokuri@gmail.com>
2017-08-29 11:35:04 +0900

    専用の言語で組み立てる：Interpreter #85
    ・15.3.2 名前でファイルを検索する
    
    EFROR-72

--- spec/domains/design_pattern/interpreter/expression_spec.rb
+++ spec/domains/design_pattern/interpreter/expression_spec.rb
@@ -9,5 +9,12 @@ describe DesignPattern::Interpreter::Expression do
       file = expr_all.evaluate(path.to_s)
       expect(file).to include("#{path.to_s}/expression_spec.rb")
     end
+
+    it 'find file by name' do
+      path = Pathname.new('./spec/domains/design_pattern/interpreter')
+      expr_spec = DesignPattern::Interpreter::FileName.new('*_spec.rb')
+      file = expr_spec.evaluate(path.to_s)
+      expect(file).to include("#{path.to_s}/expression_spec.rb")
+    end
   end
 end
