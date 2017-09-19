64ce23ceb8c3a7a594707672bb9794f31cdd6039
k2works <kakimomokuri@gmail.com>
2017-09-19 16:25:22 +0900

    通知ジョブ更新

--- app/models/message.rb
+++ app/models/message.rb
@@ -13,9 +13,25 @@ class Message < ApplicationRecord
     NotificationJob.set(wait: 5.seconds).perform_later body
   end
 
+  def logging
+    f = Fluent::Logger::LevelFluentLogger.new('fluent')
+
+    f.formatter = proc do |severity, datetime, progname, message|
+      map = { level: severity }
+      map[:message] = message if message
+      map[:progname] = progname if progname
+      map[:stage] = ENV['RAILS_ENV']
+      map[:service_name] = "SomeApp"
+      map
+    end
+
+    f.info("some_application"){"some application running."}
+  end
+
   def self.exec(message)
     msg = Message.new
     msg.body = message
+    msg.logging
     msg.save
     msg.notification
   end
