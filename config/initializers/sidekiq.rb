746dda6dd3c7119bfa9c690cf2f14e647ee2723b
k2works <kakimomokuri@gmail.com>
2017-09-30 15:12:11 +0900

    配置 SidekiqのHeroku対応

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
   case Rails.env
     when 'production' then
-      config.redis = { url: 'redis://prd.redis-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: ENV['REDIS_URL'] }
     when 'staging' then
-      config.redis = { url: 'redis://stg.redis-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: ENV['STAGING_REDIS_URL'], namespace: 'sidekiq' }
     else
       config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
   end
@@ -12,9 +12,9 @@ end
 Sidekiq.configure_client do |config|
   case Rails.env
     when 'production' then
-      config.redis = { url: 'redis://prd.redis-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: ENV['REDIS_URL'] }
     when 'staging' then
-      config.redis = { url: 'redis://stg.redis-example.com:6379', namespace: 'sidekiq' }
+      config.redis = { url: ENV['STAGING_REDIS_URL'], namespace: 'sidekiq' }
     else
       config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
   end
